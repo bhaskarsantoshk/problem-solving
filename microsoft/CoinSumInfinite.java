47c045bd5ec3fb45a017d21398fcb70cb78f0096
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-04-10 15:33:39 +0530

    microsoft/CoinSumInfinite.java

---
+++ microsoft/CoinSumInfinite.java
@@ -0,0 +1,17 @@
+package microsoft;
+
+import java.util.ArrayList;
+
+public class CoinSumInfinite {
+    public int coinchange2(ArrayList<Integer> A, int B) {
+        int dp[] = new int[B+1];
+        dp[0] = 1;
+        for(int coin : A){
+            for(int i= coin; i<=B; i++){
+                dp[i]+=dp[i-coin];
+                dp[i]%=1000007;
+            }
+        }
+        return dp[B];
+    }
+}
