07b4960074cb805499e8671b76e9aa7b69c8d1c9
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-04-25 23:40:27 +0530

    LeetCode/P965UnivaluedBinaryTree.java

--- LeetCode/P965UnivaluedBinaryTree.java
+++ LeetCode/P965UnivaluedBinaryTree.java
@@ -11,4 +11,24 @@ public class P965UnivaluedBinaryTree {
         if(root.val != v) return false;
         return dfs(root.left, v) && dfs(root.right, v);
     }
+
+    public boolean isUnivalTreeIterative(TreeNode root){
+        if(root == null){
+            return true;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode node = root;
+        while(!stack.isEmpty() || node!=null){
+            if(node!=null){
+                stack.push(node);
+                node = node.left;
+            }
+            else{
+                node = stack.pop();
+                if(node.val != root.val) return false;
+                node = node.right;
+            }
+        }
+        return true;
+    }
 }
