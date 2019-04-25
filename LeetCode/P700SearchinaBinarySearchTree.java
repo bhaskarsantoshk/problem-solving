88be4667fd05f6f5380d6609f3c84da209abac13
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-04-25 18:27:07 +0530

    LeetCode/P700SearchinaBinarySearchTree.java

---
+++ LeetCode/P700SearchinaBinarySearchTree.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class P700SearchinaBinarySearchTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null){
+            return null;
+        }
+        if(root.val == val){
+            return root;
+        }
+        if(root.val > val){
+            return searchBST(root.left, val);
+        }
+        else{
+            return searchBST(root.right, val);
+        }
+    }
+}
