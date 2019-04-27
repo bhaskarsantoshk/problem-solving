73e757ab838c463655e1ae57f0411b2830668291
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-04-27 12:03:00 +0530

    sql/P182DuplicateEmails

--- sql/P620NotBoringMovies.sql
+++ sql/P620NotBoringMovies.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-SELECT id, movie, description , rating FROM cinema WHERE id %2 != 0 and description != "boring" ORDER BY rating DESC;
+SELECT id, movie, description , rating
+FROM cinema
+WHERE id %2 != 0 and description != "boring"
+ORDER BY rating DESC;
