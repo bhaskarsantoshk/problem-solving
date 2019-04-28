c0ddec74d39e19ee5ab10e80bda6472b07cf14c8
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-04-29 00:30:41 +0530

    LeetCode/P929UniqueEmailAddresses.java

--- LeetCode/P929UniqueEmailAddresses.java
+++ LeetCode/P929UniqueEmailAddresses.java
@@ -1,7 +1,16 @@
 package LeetCode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class P929UniqueEmailAddresses {
     public int numUniqueEmails(String[] emails) {
-
+        Set<String> uniqueEmailSet = new HashSet<String>();
+        for(String email: emails){
+            String[] parts = email.split("@");
+            String[] local = parts[0].split("\\+");
+            uniqueEmailSet.add(local[0].replace(".", "")+"@"+parts[1]);
+        }
+        return uniqueEmailSet.size();
     }
 }
