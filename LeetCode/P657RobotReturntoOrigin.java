2852aedc8a1377b2512bc81103ec689f4be124b0
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-04-30 00:22:50 +0530

    LeetCode/P657RobotReturntoOrigin.java

---
+++ LeetCode/P657RobotReturntoOrigin.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class P657RobotReturntoOrigin {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y= 0;
+        for(char move= moves.toCharArray()){
+            switch(move){
+                case 'L':
+                    x--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+            }
+        }
+        return x== 0 && y==0;
+    }
+}
