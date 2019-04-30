6ce891876c34dd5361dd3b426d28a2dbdbaa88cf
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-04-30 23:56:29 +0530

    LeetCode/P701InsertintoaBinarySearchTree.java

---
+++ LeetCode/P701InsertintoaBinarySearchTree.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+public class P701InsertintoaBinarySearchTree {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null ) return new TreeNode(val);
+        if(root.val > val){
+            root.left = insertIntoBST(root.left, val);
+        }
+        else{
+            root.right = insertIntoBST(root.right , val);
+        }
+        return root;
+    }
+}
