f17cc74fd1aae4340e8522980e891b33d7ed40a9
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-05-01 14:46:02 +0530

    LeetCode/P450DeletenodeinaBST.java

---
+++ LeetCode/P450DeletenodeinaBST.java
@@ -0,0 +1,43 @@
+package LeetCode;
+
+public class P450DeletenodeinaBST {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if( root == null ) return null;
+        if ( key > root.val ){
+            root.right = deleteNode(root.right, key);
+        }
+        else if ( key < root.val ){
+            root.left = deleteNode(root.left, key);
+        }
+        else{
+            if(root.left == null && root.right == null){
+                root = null;
+            }
+            else if (root.left != null){
+                root.val = predecessor(root);
+                root.left = deleteNode(root.left, root.val);
+            }
+            else {
+                root.val = successor(root);
+                root.right = deleteNode(root.right, root.val);
+            }
+        }
+        return  root;
+    }
+
+    private int successor(TreeNode root) {
+        root = root.right;
+        while(root.left != null ){
+            root = root.left;
+        }
+        return root.val;
+    }
+
+    private int predecessor(TreeNode root) {
+        root = root.left;
+        while(root.right != null ){
+            root = root.right;
+        }
+        return root.val;
+    }
+}
