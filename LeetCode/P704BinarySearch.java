685962b69e1a39b08591fa6bf6c27cdbcb9e33b7
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-05-02 09:10:44 +0530

    LeetCode/P704BinarySearch.java

---
+++ LeetCode/P704BinarySearch.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+public class P704BinarySearch {
+    public int search(int[] nums, int target) {
+        if( nums == null || nums.length == 0) return -1;
+        int start = 0;
+        int end = nums.length-1;
+        while( start <= end ){
+            int mid = start + (end - start)/2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+}
