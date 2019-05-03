ac41e37cf8dbd256f14b5ddf50994e22340b361b
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-05-03 10:35:16 +0530

    LeetCode/P73SetMatrixZeroes.java

--- LeetCode/P73SetMatrixZeroes.java
+++ LeetCode/P73SetMatrixZeroes.java
@@ -1,4 +1,23 @@
 package LeetCode;
 
 public class P73SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        int rows[] = new int[matrix.length];
+        int columns[] = new int[matrix[0].length];
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = 1;
+                    columns[j] = 1;
+                }
+            }
+        }
+        for(int i=0; i<matrix.length ; i++){
+            for(int j=0; j<matrix[0].length ; j++){
+                if (rows[i]== 1 || columns[j]==1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
 }
