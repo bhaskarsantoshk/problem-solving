80f7fe7392f66a6b6fca6262e6b400a65333b50e
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-05-07 22:00:47 +0530

    LeetCode/P328OddEvenLinkedList.java

---
+++ LeetCode/P328OddEvenLinkedList.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+public class P328OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        if(head!= null) {
+            ListNode odd = head;
+            ListNode even = head.next;
+            ListNode evenHead = even;
+            while (even != null && even.next!= null){
+                odd.next=  odd.next.next;
+                even.next= even.next.next;
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+        }
+        return head;
+    }
+}
