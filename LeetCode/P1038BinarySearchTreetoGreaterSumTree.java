ec42f66ed1119fdfcd372d4ad96456a7b9c05965
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-05-07 09:43:03 +0530

    LeetCode/P1038BinarySearchTreetoGreaterSumTree.java

---
+++ LeetCode/P1038BinarySearchTreetoGreaterSumTree.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+public class P1038BinarySearchTreetoGreaterSumTree {
+    int pre = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if( root.right != null ) bstToGst(root.right);
+        pre = root.val = pre + root.val;
+        if(root.left != null) bstToGst(root.left);
+        return root;
+    }
+}
