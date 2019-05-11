ac3d8572da8b4bb36fd6a2bd32f59f2c503fbaa5
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-05-11 12:56:03 +0530

    LeetCode/P543DiameterofBinaryTree.java

---
+++ LeetCode/P543DiameterofBinaryTree.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+public class P543DiameterofBinaryTree {
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+    private int maxDepth(TreeNode root) {
+        if (root == null ) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        max = Math.max(max, left+right);
+        return Math.max(left, right)+1;
+    }
+}
