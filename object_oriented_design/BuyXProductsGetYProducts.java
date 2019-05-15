dae35b2380379399d7e9290797d326e9a1b3884e
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-05-15 19:14:29 +0530

    object_oriented_design/BuyXProductsGetYProducts.java

--- object_oriented_design/BuyXProductsGetYProducts.java
+++ object_oriented_design/BuyXProductsGetYProducts.java
@@ -7,7 +7,7 @@ public class BuyXProductsGetYProducts extends Rule{
     private double totalCostUponRuleApplied;
     private String productCode;
 
-    public BuyXProductsGetYProducts(int x, int y, int numberOfProductsPurchased, String productCode) {
+    public BuyXProductsGetYProducts(int x, int y, String productCode) {
         this.x = x;
         this.y = y;
         this.numberOfProductsPurchased = numberOfProductsPurchased;
@@ -15,6 +15,9 @@ public class BuyXProductsGetYProducts extends Rule{
     }
 
     public double apply(String product_code, int numberOfProductsPurchased, double price ) {
+        if(!productCode.equals(product_code)) {
+            return 0;
+        }
         if(productCode.equals(product_code) &&
                 numberOfProductsPurchased >= x){
             int productsForWhichPriceIsApplicable = (numberOfProductsPurchased / (x+y)) * x +
@@ -22,7 +25,7 @@ public class BuyXProductsGetYProducts extends Rule{
             totalCostUponRuleApplied = productsForWhichPriceIsApplicable * price ;
         }
         else{
-            totalCostUponRuleApplied = numberOfProductsPurchased * price;
+            totalCostUponRuleApplied = -1;
         }
         return totalCostUponRuleApplied;
     }
