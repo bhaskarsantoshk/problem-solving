73b67f4d4af9e1a7490e518e70df224f98fd4813
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-05-18 15:09:42 +0530

    LeetCode/P3LongestSubstringWithoutRepeatingCharacters.java

--- LeetCode/P3LongestSubstringWithoutRepeatingCharacters.java
+++ LeetCode/P3LongestSubstringWithoutRepeatingCharacters.java
@@ -5,18 +5,21 @@ import java.util.Set;
 
 public class P3LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
-        Set<Character> set = new HashSet<>();
-        int i=0, j=0, max = 0;
-
-        while(j<s.length()){
-            if(!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
-                max = Math.max(max, set.size());
-            }
-            else{
-                set.remove(s.charAt(i++));
-            }
-        }
-        return max;
+       int i=0, j=0;
+       int maxLen=0;
+       Set<Character> set = new HashSet<>();
+       while( j < s.length()){
+           char c= s.charAt(j);
+           if(!set.contains(c)){
+               set.add(c);
+               j++;
+               maxLen = Math.max(maxLen, set.size());
+           }
+           else {
+               set.remove(s.charAt(i));
+               i++;
+           }
+       }
+       return maxLen;
     }
 }
