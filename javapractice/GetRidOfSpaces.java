57d2c08bc177fdb8986e202999dac8f8b8b71dd3
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-05-27 02:11:03 +0530

    javapractice/GetRidOfSpaces.java

---
+++ javapractice/GetRidOfSpaces.java
@@ -0,0 +1,19 @@
+package javapractice;
+
+import java.util.StringTokenizer;
+
+public class GetRidOfSpaces {
+    public static void main(String args[]){
+
+        String input = "Try    to    remove   extra   spaces.";
+        StringTokenizer substr = new StringTokenizer(input, " ");
+        StringBuffer sb = new StringBuffer();
+
+        while(substr.hasMoreElements()){
+            sb.append(substr.nextElement()).append(" ");
+        }
+
+        System.out.println("Actual string: " + input);
+        System.out.println("Processed string: " + sb.toString().trim());
+    }
+}
