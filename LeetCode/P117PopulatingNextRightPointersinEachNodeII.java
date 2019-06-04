2b64ddb04d6652fb0a21c2f9e58c8e7d4904d439
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-06-04 13:40:12 +0530

    LeetCode/P117PopulatingNextRightPointersinEachNodeII.java

--- LeetCode/P117PopulatingNextRightPointersinEachNodeII.java
+++ LeetCode/P117PopulatingNextRightPointersinEachNodeII.java
@@ -33,12 +33,14 @@ public class P117PopulatingNextRightPointersinEachNodeII {
         public Node right;
         public Node next;
 
-        public Node() {}
+        public Node() {
+        }
 
-        public Node(int _val,Node _left,Node _right,Node _next) {
+        public Node(int _val, Node _left, Node _right, Node _next) {
             val = _val;
             left = _left;
             right = _right;
             next = _next;
         }
+    }
 }
