582aae926bd2b32b81c80b340e8567ed3eed55e5
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-06-04 21:04:03 +0530

    LeetCode/P19RemoveNthNodeFromEndofList.java

--- LeetCode/P19RemoveNthNodeFromEndofList.java
+++ LeetCode/P19RemoveNthNodeFromEndofList.java
@@ -2,17 +2,18 @@ package LeetCode;
 
 public class P19RemoveNthNodeFromEndofList {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode start = new ListNode(0);
-        ListNode slow = start, fast = start;
-        slow.next = head;
-        for(int i=1;i<=n+1; i++){
+        ListNode fast = head;
+        ListNode tempHead = new ListNode(0);
+        for(int i=1; i<= n; i++){
             fast = fast.next;
         }
+        ListNode slow = tempHead;
+        slow.next = head;
         while ( fast != null ){
-            slow = slow.next;
             fast = fast.next;
+            slow = slow.next;
         }
         slow.next = slow.next.next;
-        return start.next;
+        return tempHead.next;
     }
 }
