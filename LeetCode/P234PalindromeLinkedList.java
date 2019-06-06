f4f752fa0cbc01d5a32a90924ecfa51adfe1778b
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-06-06 12:47:13 +0530

    LeetCode/P234PalindromeLinkedList.java

--- LeetCode/P234PalindromeLinkedList.java
+++ LeetCode/P234PalindromeLinkedList.java
@@ -2,35 +2,39 @@ package LeetCode;
 
 public class P234PalindromeLinkedList {
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next== null ) return true;
+        if ( head == null ) return true;
         ListNode slow = head;
         ListNode fast = head;
-        while(fast.next != null && fast.next.next != null){
+        while (fast.next != null && fast.next.next != null ){
             slow = slow.next;
             fast = fast.next.next;
         }
-        ListNode newHead = slow.next;
-        ListNode a = reverseList(newHead);
-        ListNode b = head;
+        if ( fast.next != null ){
+            slow = slow.next;
+        }
 
-        while(a!= null && b!= null){
-            if(a.val != b.val){
+        ListNode a = head;
+        ListNode b = reverseList(slow);
+
+        while ( a!= null && b!= null ){
+            if (a.val != b.val){
                 return false;
             }
-            a= a.next;
-            b= b.next;
+            a = a.next;
+            b = b.next;
         }
-        return true;
 
+        return true;
     }
 
     private ListNode reverseList(ListNode head) {
         ListNode newHead = null;
-        while(head != null){
-            ListNode next = head.next;
-            head.next = newHead;
-            newHead = head;
-            head = next;
+        ListNode current = head;
+        while ( current != null ){
+            ListNode next = current.next;
+            current.next = newHead;
+            newHead = current;
+            current = next;
         }
         return newHead;
     }
