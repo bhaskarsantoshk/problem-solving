f0dc8e30f73d85859087ae912da4e2882151685d
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-06-08 01:48:44 +0530

    LeetCode/P98ValidateBinarySearchTree.java

---
+++ LeetCode/P98ValidateBinarySearchTree.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+public class P98ValidateBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long minValue, long maxValue) {
+        if ( root == null ) return true;
+        if ( root.val < minValue || root.val > maxValue )return false;
+        return isValidBST(root.left, minValue, root.val) &&
+                isValidBST(root.right, root.val, maxValue);
+    }
+}
