c7baccfc62c36ef7d8626cef8c080c127ebb35f8
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-06-13 17:31:20 +0530

    LeetCode/P145BinaryTreePostorderTraversal.java

--- LeetCode/P145BinaryTreePostorderTraversal.java
+++ LeetCode/P145BinaryTreePostorderTraversal.java
@@ -1,7 +1,9 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class P145BinaryTreePostorderTraversal {
     public List<Integer> postorderTraversal(TreeNode root) {
@@ -21,6 +23,39 @@ public class P145BinaryTreePostorderTraversal {
             postOrder(root.right, list);
         }
         list.add(root.val);
+
+    }
+
+    public List<Integer> postOrderIterative( TreeNode root ){
+     List<Integer> list = new ArrayList<>();
+     if ( root == null ) return list;
+     Stack<TreeNode> stack = new Stack<>();
+     TreeNode node = root;
+
+     while ( !stack.isEmpty() || node != null ){
+         if ( node != null ){
+             stack.push(node);
+             node = node.left;
+         }
+         else{
+             TreeNode temp = stack.peek().right;
+             if ( temp == null ) {
+                 temp = stack.pop();
+                 list.add(temp.val);
+
+                 while (!stack.isEmpty() && temp == stack.peek().right) {
+                     temp = stack.pop();
+                     list.add(temp.val);
+                 }
+             }
+             else{
+                 node = temp;
+             }
+             }
+         }
+
+         return list;
+     }
 
     }
 }
