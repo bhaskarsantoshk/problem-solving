ea3c5b27cdc63fee1dc8e5bfbdb3e6a9ee8c8407
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-06-13 20:57:41 +0530

    LeetCode/P236LowestCommonAncestorofaBinaryTree.java

---
+++ LeetCode/P236LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+public class P236LowestCommonAncestorofaBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if ( root == null || root == p || root == q ){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if ( left != null && right != null ){
+            return root;
+        }
+        return left != null ? left : right;
+    }
+}
