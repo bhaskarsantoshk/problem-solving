72f4ac349671839386cf0ad52598eee8ab37b33f
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-06-18 01:23:01 +0530

    LeetCode/P258AddDigits.java

---
+++ LeetCode/P258AddDigits.java
@@ -0,0 +1,9 @@
+package LeetCode;
+
+public class P258AddDigits {
+    public int addDigits(int num) {
+        if ( num == 0 ) return 0;
+        if ( num % 9 == 0 ) return 9;
+        return num % 9;
+    }
+}
