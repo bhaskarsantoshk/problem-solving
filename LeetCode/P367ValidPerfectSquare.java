3118d207d0012cbb986fcb9f05460e8cd0d990ab
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-06-18 01:35:48 +0530

    LeetCode/P367ValidPerfectSquare.java

---
+++ LeetCode/P367ValidPerfectSquare.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class P367ValidPerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        int i = 1;
+        while ( num > 0){
+            num -=i;
+            i+=2;
+        }
+        return num == 0;
+    }
+}
