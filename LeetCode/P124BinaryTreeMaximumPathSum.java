6424f1d8055760ae5d28b5a02f515b9886c7437a
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-07-12 17:02:26 +0530

    LeetCode/P124BinaryTreeMaximumPathSum.java

--- LeetCode/P124BinaryTreeMaximumPathSum.java
+++ LeetCode/P124BinaryTreeMaximumPathSum.java
@@ -1,18 +1,26 @@
 package LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P124BinaryTreeMaximumPathSum {
     int max;
+    Map<TreeNode, Integer> map = new HashMap<TreeNode, Integer>();
     public int maxPathSum(TreeNode root) {
-        max= Integer.MIN_VALUE;
-        maxPathDown(root);
+        max = Integer.MIN_VALUE;
+        maxPath(root);
         return max;
     }
-
-    private int maxPathDown(TreeNode root) {
-        if( root == null ) return 0;
-        int left = Math.max( 0, maxPathDown(root.left));
-        int right = Math.max( 0, maxPathDown(root.right));
-        max = Math.max(max, left + right+ root.val);
-        return Math.max(left, right) + root.val;
+    private int maxPath(TreeNode root) {
+        if ( root == null ) return 0;
+        if ( map.containsKey(root)){
+            return map.get(root);
+        }
+        int left = Math.max ( 0, maxPath(root.left));
+        int right = Math.max ( 0, maxPath(root.right));
+        max = Math.max ( max, left + right + root.val);
+        int result = Math.max ( left, right) + root.val;
+        map.put(root, result);
+        return result;
     }
 }
