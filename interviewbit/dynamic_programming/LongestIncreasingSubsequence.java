fcef2e067d9297ea12d5536389a8d2cc9123c8ff
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-07-13 12:24:43 +0530

    interviewbit/dynamic_programming/LongestIncreasingSubsequence.java

--- interviewbit/dynamic_programming/LongestIncreasingSubsequence.java
+++ interviewbit/dynamic_programming/LongestIncreasingSubsequence.java
@@ -6,28 +6,43 @@ public class LongestIncreasingSubsequence {
     public static void main(String args[]){
         LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
         int arr[] = {23,10,22,5,33,8,9,21,50,41,60,80,99, 22,23,24,25,26,27};
-        int result = lis.longestSubsequenceDP(arr);
+        int result = lis.longestSubsequenceDPNSquare(arr);
         int result1 = lis.longestSubsequenceRecursive(arr);
         System.out.println(result);
         System.out.println(result1);
     }
 
     private int longestSubsequenceRecursive(int[] arr) {
-        return 0;
+        return lengthOfLIS(arr, Integer.MIN_VALUE, 0);
     }
 
-    private int longestSubsequenceDP(int[] arr) {
-        int res[] = new int[arr.length];
-        int result = 0;
-        Arrays.fill(arr,1);
-        for(int i=1 ; i<arr.length ; i++){
-            for(int j=0; j<i ; j++){
-                if(arr[i] > arr[j] && res[j]+1 > res[i]){
-                    res[i] = res[j]+1;
-                    result = Math.max(res[i], result);
+    private int lengthOfLIS(int[] arr, int prev, int index) {
+        if ( index == arr.length ){
+            return 0;
+        }
+        int taken = 0;
+        if ( arr[index] > prev ){
+            taken = 1 + lengthOfLIS(arr, arr[index], index + 1);
+        }
+        int nottaken = lengthOfLIS(arr, prev, index+ 1);
+        return Math.max ( taken, nottaken);
+    }
+
+    private int longestSubsequenceDPNSquare(int[] arr) {
+        if ( arr == null || arr.length == 0 ){
+            return 0;
+        }
+        int lis[] = new int[arr.length];
+        Arrays.fill(lis, 1);
+        int max = 1;
+        for ( int i = 0; i < arr.length ; i++ ){
+            for ( int j= 0; j <= i ; j++ ){
+                if ( arr[i] > arr[j] && lis[j] + 1 > lis[i]){
+                    lis[i] = lis[j] + 1;
+                    max = Math.max ( max, lis[i]);
                 }
             }
         }
-        return result;
+        return max;
     }
 }
