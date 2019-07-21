bdd55e40a956f8221786840468332056c99aad7b
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-07-21 19:53:05 +0530

    interviewbit/dynamic_programming/TreeNode.java

---
+++ interviewbit/dynamic_programming/TreeNode.java
@@ -0,0 +1,13 @@
+package interviewbit.dynamic_programming;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x){
+        this.val = x;
+        left = null;
+        right = null;
+    }
+}
