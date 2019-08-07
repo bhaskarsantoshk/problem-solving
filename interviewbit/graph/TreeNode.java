0ef2695ae52ba050a235c0e5be7083723f4f22a0
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-08-07 23:37:08 +0530

    interviewbit/graph/TreeNode.java

---
+++ interviewbit/graph/TreeNode.java
@@ -0,0 +1,13 @@
+package interviewbit.graph;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x){
+        val = x;
+        left = null;
+        right = null;
+    }
+}
