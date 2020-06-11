8b8431ae9fdeae33e8ee8c44effef5543e1aa7a1
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2020-06-11 18:04:18 +0530

    Update SumRoottoLeafNumbers.java

--- company/microsoft/SumRoottoLeafNumbers.java
+++ company/microsoft/SumRoottoLeafNumbers.java
@@ -4,7 +4,6 @@ public class SumRoottoLeafNumbers {
     public int sumNumbers(TreeNode A) {
         return sum(A, 0);
     }
-
     private int sum(TreeNode root, int s) {
         if(root == null){
             return 0;
@@ -13,7 +12,6 @@ public class SumRoottoLeafNumbers {
         if(root.left == null && root.right== null){
             return (s*10 + root.val)%1003;
         }
-
         return sum(root.left, (s*10+root.val)%1003) + sum(root.right, (s*10+root.val)%1003);
     }
 }
