bdf1cb69b2375c5bc1286851c1b15c70c1c8807d
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2020-06-16 19:28:41 +0530

    LeetCode/P1323Maximum69Number.java

---
+++ LeetCode/P1323Maximum69Number.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class P1323Maximum69Number {
+    public int maximum69Number (int num) {
+        String s = String.valueOf(num);
+        char c[] = s.toCharArray();
+
+        boolean flag = false;
+        for ( int i=0; i<c.length; i++){
+            if (c[i]=='6'){
+                c[i] ='9';
+                break;
+            }
+        }
+        return Integer.parseInt(String.valueOf(c));
+    }
+}
