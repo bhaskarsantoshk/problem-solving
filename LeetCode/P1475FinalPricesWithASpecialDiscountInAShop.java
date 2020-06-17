3fcd18244e00cb37e5961bcba6e29aa472e108aa
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2020-06-17 23:08:33 +0530

    LeetCode/P1475FinalPricesWithASpecialDiscountInAShop.java

---
+++ LeetCode/P1475FinalPricesWithASpecialDiscountInAShop.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+public class P1475FinalPricesWithASpecialDiscountInAShop {
+    public int[] finalPrices(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i])
+                prices[stack.pop()] -= prices[i];
+            stack.push(i);
+        }
+        return prices;
+    }
+}
