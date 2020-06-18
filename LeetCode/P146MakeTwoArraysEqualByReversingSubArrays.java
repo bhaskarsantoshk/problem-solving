dde2e7d465500b4d5adafbecffcfbd8e889498ff
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2020-06-18 22:33:41 +0530

    LeetCode/P146MakeTwoArraysEqualByReversingSubArrays.java

---
+++ LeetCode/P146MakeTwoArraysEqualByReversingSubArrays.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+public class P146MakeTwoArraysEqualByReversingSubArrays {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+
+        for ( int i=0; i<arr.length; i++){
+            if (arr[i] != target[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
