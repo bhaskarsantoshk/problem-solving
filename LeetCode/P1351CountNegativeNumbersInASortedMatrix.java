fd3a1390f1394121ecefbba09a84f3a490470f1a
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2020-06-24 20:46:40 +0530

    LeetCode/P44SumOfLeftLeaves.java

--- LeetCode/P1351CountNegativeNumbersInASortedMatrix.java
+++ LeetCode/P1351CountNegativeNumbersInASortedMatrix.java
@@ -5,7 +5,6 @@ public class P1351CountNegativeNumbersInASortedMatrix {
         int rows = grid.length, cols = grid[0].length;
         int rowIndex = rows-1, colIndex = 0;
         int res = 0;
-
         while (rowIndex>=0 && colIndex <cols){
             if (grid[rowIndex][colIndex] < 0){
                 res += cols - colIndex;
