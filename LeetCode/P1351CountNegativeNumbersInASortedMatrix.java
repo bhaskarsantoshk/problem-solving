279f80acc1782dc2b5a9cc33c056a7d46c41340b
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2020-06-25 20:46:20 +0530

    LeetCode/P1351CountNegativeNumbersInASortedMatrix.java

---
+++ LeetCode/P1351CountNegativeNumbersInASortedMatrix.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class P1351CountNegativeNumbersInASortedMatrix {
+    public int countNegatives(int[][] grid) {
+        int rows = grid.length, cols = grid[0].length;
+        int rowIndex = rows-1, colIndex = 0;
+        int res = 0;
+
+        while (rowIndex>=0 && colIndex <cols){
+            if (grid[rowIndex][colIndex] < 0){
+                res += cols - colIndex;
+                rowIndex--;
+            }
+            else{
+                colIndex++;
+            }
+        }
+        return res;
+    }
+}
