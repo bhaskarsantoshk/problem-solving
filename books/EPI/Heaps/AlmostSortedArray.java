b6b12a40e947087e78614d3b274a8ea0bf5715d8
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2020-08-01 21:26:27 +0530

    Create AlmostSortedArray.java

---
+++ books/EPI/Heaps/AlmostSortedArray.java
@@ -0,0 +1,37 @@
+package books.EPI.Heaps;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AlmostSortedArray {
+    public static List<Integer> sortAlmostSortedArray (Iterator<Integer> iterator, int k){
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+        List<Integer> result = new ArrayList<>();
+        while ( iterator.hasNext() ){
+            minHeap.add(iterator.next());
+            if ( minHeap.size() > k){
+                result.add(minHeap.poll());
+            }
+        }
+        //result.addAll(new ArrayList<>(minHeap));
+        result.addAll(Stream.generate(minHeap::remove).limit(minHeap.size()).collect(Collectors.toList()));
+        return result;
+    }
+
+    public static void main(String[] args){
+        List<Integer> a = new ArrayList<>();
+        a.add(3);
+        a.add(-1);
+        a.add(2);
+        a.add(6);
+        a.add(4);
+        a.add(5);
+        a.add(8);
+        System.out.println(sortAlmostSortedArray(a.iterator(), 2));
+    }
+}
