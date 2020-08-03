7e2912af07a78591dfe377e47dd023d04ec26f40
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2020-08-03 22:03:11 +0530

    Create BinarySearch.java

---
+++ books/EPI/Searching/BinarySearch.java
@@ -0,0 +1,22 @@
+package books.EPI.Searching;
+
+import java.util.List;
+
+public class BinarySearch {
+    public static int binarySearch(int key, List<Integer> A){
+        int start = 0, end = A.size()-1;
+        while ( start <= end ){
+            int mid = (start+end)/2;
+            if ( A.get(mid) == key){
+                return mid;
+            }
+            else if ( A.get(mid) < key ){
+                start = mid+1;
+            }
+            else {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+}
