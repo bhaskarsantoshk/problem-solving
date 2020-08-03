23ce107a816290efa6691bff9028a208d912e432
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2020-08-03 22:04:17 +0530

    Update BinarySearch.java

--- books/EPI/Searching/BinarySearch.java
+++ books/EPI/Searching/BinarySearch.java
@@ -6,7 +6,8 @@ public class BinarySearch {
     public static int binarySearch(int key, List<Integer> A){
         int start = 0, end = A.size()-1;
         while ( start <= end ){
-            int mid = (start+end)/2;
+            //int mid = (start+end)/2;
+            int mid = start + (end-start)/2;
             if ( A.get(mid) == key){
                 return mid;
             }
