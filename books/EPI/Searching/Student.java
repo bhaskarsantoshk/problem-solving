899f4f6c8848cac36deea19d49df55123707f319
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2020-08-03 22:13:30 +0530

    Create Student.java

---
+++ books/EPI/Searching/Student.java
@@ -0,0 +1,11 @@
+package books.EPI.Searching;
+
+public class Student {
+    public String name;
+    public double gradePointAverage;
+
+    public Student(String name, double gradePointAverage){
+        this.name = name;
+        this.gradePointAverage = gradePointAverage;
+    }
+}
