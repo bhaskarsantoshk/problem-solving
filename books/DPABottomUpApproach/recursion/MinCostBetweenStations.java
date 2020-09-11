ae313b9aa10863bf8e0fb33cc09fe5b78ddd58d2
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2020-09-11 15:27:26 +0530

    Create MinCostBetweenStations.java

---
+++ books/DPABottomUpApproach/recursion/MinCostBetweenStations.java
@@ -0,0 +1,19 @@
+package books.DPABottomUpApproach.recursion;
+
+public class MinCostBetweenStations {
+
+    int[][] cost;
+
+    int calculateMinCost( int s, int d){
+        if ( s == d || s == d-1) return cost[s][d];
+        int minCost = Interger.MAX_VALUE;
+        for ( int i=s+1; i<d; i++){
+          int temp = calculateMinCost(s,i)+ calculateMinCost(i+1, d);
+          minCost = Math.min(minCost, temp);
+        }
+        
+        return minCost;
+    }
+
+
+}
