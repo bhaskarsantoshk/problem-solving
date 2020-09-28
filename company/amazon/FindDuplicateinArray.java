b0d6422922a1692affe0d54a2d89d1bd46a53498
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2020-09-28 23:59:24 +0530

    Create FindDuplicateinArray.java

---
+++ company/amazon/FindDuplicateinArray.java
@@ -0,0 +1,18 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST
+    public int repeatedNumber(final List<Integer> a) {
+      if ( a == null || a.size() == 0 || a.size() == 1) return -1;
+      int slow = a.get(0);
+      int fast = a.get(0);
+      do{
+          slow = a.get(slow);
+          fast = a.get(a.get(fast));
+      }while(slow != fast);
+      slow = 0;
+      while(slow != fast){
+          slow = a.get(slow);
+          fast = a.get(fast);
+      }
+      return slow;
+    }
+}
