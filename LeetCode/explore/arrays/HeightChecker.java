6fb68c4a0dc9d781f7c66be1d9df1d85753a2293
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-01-12 19:36:45 +0530

    LeetCode/explore/arrays/HeightChecker.java

--- LeetCode/explore/arrays/HeightChecker.java
+++ LeetCode/explore/arrays/HeightChecker.java
@@ -3,14 +3,14 @@ package LeetCode.explore.arrays;
 public class HeightChecker {
 
     public int heightChecker(int[] heights) {
-        int count[] = new int[heights.length+1];
+        int count[] = new int[1000];
         for ( int height: heights){
             count[height]++;
         }
         int res = 0;
         int currentHeight = 0;
         for ( int i=0; i< heights.length ; i++){
-            while ( heights[currentHeight] == 0){
+            while ( count[currentHeight] == 0){
                 currentHeight++;
             }
             if ( currentHeight != heights[i]){
