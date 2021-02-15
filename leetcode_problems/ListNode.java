7e91e01e31500abaf08bbd97f7e27270b4bce449
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-02-15 13:46:51 +0530

    leetcode_problems/ListNode.java

---
+++ leetcode_problems/ListNode.java
@@ -0,0 +1,18 @@
+package leetcode_problems;
+
+import java.util.List;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){
+
+    }
+    ListNode(int val){
+
+    }
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
