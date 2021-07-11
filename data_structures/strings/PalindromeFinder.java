4aa308177003278d7f7638acfe07f8a887185224
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-07-11 15:53:57 +0530

    data_structures/strings/PalindromeFinder.java

--- data_structures/strings/PalindromeFinder.java
+++ data_structures/strings/PalindromeFinder.java
@@ -7,6 +7,7 @@ public class PalindromeFinder {
         for ( int i=0; i<s.length(); i++){
             for (int j=i+1; j<s.length(); j++){
                 if ( isPalindrome(s, i, j)){
+                    System.out.println(s.substring(i,j+1));
                     count++;
                 }
             }
@@ -23,5 +24,34 @@ public class PalindromeFinder {
             end--;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "aabbbaa";
+        int count = getAllPalindromesBruteForce(str);
+        System.out.println("Total palindrome substrings: " + count);
+    }
+
+    // Better Solution
+    public static int getPalindromes ( String s ){
+        if ( s == null || s.length() == 0) return 0;
+        int count = 0;
+        for ( int i =0; i<s.length(); i++){
+            count+= getPalindromes(s, i-1, i+1);
+            count+= getPalindromes(s, i, i+1);
+        }
+
+    }
+
+    private static int getPalindromes(String s, int j, int k) {
+        int count = 0;
+        for ( ; j>=0 && k<s.length(); j--, k++){
+            if ( s.charAt(j) != s.charAt(k)){
+                break;
+            }
+            System.out.println(s.substring(j,k+1));
+            count++;
+        }
+        return count;
     }
 }
