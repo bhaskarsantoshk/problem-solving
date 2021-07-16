44c22b5c1024c0668cc4811c1456d132ae5d2957
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-07-16 23:03:10 +0530

    LeetCode/explore/top_interview_questions/easy/ListNode.java

---
+++ LeetCode/explore/top_interview_questions/easy/ListNode.java
@@ -0,0 +1,10 @@
+package LeetCode.explore.top_interview_questions.easy;
+
+public class ListNode {
+    int data;
+    ListNode next;
+
+    public ListNode(int data){
+        this.data = data;
+    }
+}
