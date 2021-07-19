b3eced5a9da7257ba393c76782f48986809f4307
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-07-19 21:40:50 +0530

    data_structures/strings/PalindromePartitioning.java

--- data_structures/strings/PalindromePartitioning.java
+++ data_structures/strings/PalindromePartitioning.java
@@ -28,11 +28,12 @@ public class PalindromePartitioning {
 
     public int getMinCutsForPalindromeDP (String s){
         Integer[][] memo = new Integer[s.length()+1][s.length()+1];
-        return getMinCutsForPalindromeDP(memo, s, 0, s.length()-1);
+        Boolean[][] memoPalindrome = new Boolean[s.length()+1][s.length()+1];
+        return getMinCutsForPalindromeDP(memo, memoPalindrome, s, 0, s.length()-1);
     }
 
-    private int getMinCutsForPalindromeDP(Integer[][] memo, String s, int start, int end) {
-        if (start >= end || isPalindrome(s, start, end)){
+    private int getMinCutsForPalindromeDP(Integer[][] memo, Boolean[][] memoPalindrome, String s, int start, int end) {
+        if (start >= end || isPalindromeDP(memoPalindrome,s, start, end)){
             return 0;
         }
         if ( memo[start][end] == null ){
@@ -45,5 +46,23 @@ public class PalindromePartitioning {
             memo[start][end] = minCuts;
         }
         return memo[start][end];
+    }
+
+    private boolean isPalindromeDP(Boolean[][] memoPalindrome, String s, int start, int end) {
+        if ( memoPalindrome[start][end] == null ){
+            memoPalindrome[start][end] = true;
+            int left = start, right = end;
+            while ( left < end){
+                if(s.charAt(left++) != s.charAt(right--)) {
+                    memoPalindrome[left][right]=false;
+                    break;
+                }
+                if(left < end && memoPalindrome[left][right] != null) {
+                    memoPalindrome[start][end] = memoPalindrome[left][right];
+                    break;
+                }
+            }
+        }
+        return memoPalindrome[start][end];
     }
 }
