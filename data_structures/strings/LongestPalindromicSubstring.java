5e545d335cc24e32508426e371678639756c4b1d
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-07-19 20:06:50 +0530

    data_structures/strings/LongestPalindromicSubstring.java

--- data_structures/strings/LongestPalindromicSubstring.java
+++ data_structures/strings/LongestPalindromicSubstring.java
@@ -18,4 +18,27 @@ public class LongestPalindromicSubstring {
         int c2 = findLPSLengthRec(s, start, end-1);
         return Math.max(c1,c2);
     }
+
+    public int findLPSMemo( String s){
+        Integer[][] memo = new Integer[s.length()+1][s.length()+1];
+        return findLPSMemo(memo, s, 0, s.length()-1);
+    }
+
+    private int findLPSMemo(Integer[][] memo, String s, int start, int end) {
+        if ( start > end) return 0;
+        if ( start == end) return 1;
+        if (memo[start][end] == null ){
+            if ( s.charAt(start) == s.charAt(end)){
+                int remLen = end-start+1;
+                if ( remLen == findLPSMemo(memo, s, start+1, end-1)){
+                    memo[start][end] = remLen+2;
+                    return memo[start][end];
+                }
+            }
+            int c1 = findLPSMemo(memo, s, start+1, end);
+            int c2 = findLPSMemo(memo, s, start, end-1);
+            memo[start][end] = Math.max(c1, c2);
+        }
+        return memo[start][end];
+    }
 }
