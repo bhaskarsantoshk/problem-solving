db6371b44390a20ae508c6645d70dea928d571b4
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-07-24 11:11:53 +0530

    data_structures/trees/TreeNode.java

---
+++ data_structures/trees/TreeNode.java
@@ -0,0 +1,11 @@
+package data_structures.trees;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data){
+        this.data = data;
+    }
+}
