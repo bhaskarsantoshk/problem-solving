8d92890a4c2d03a6415c558b9b58405dd4b9363c
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-07-24 11:48:04 +0530

    data_structures/trees/InorderTraversal.java

--- data_structures/trees/InorderTraversal.java
+++ data_structures/trees/InorderTraversal.java
@@ -17,4 +17,11 @@ public class InorderTraversal {
             }
         }
     }
+
+    public void inorder(TreeNode root){
+        if ( root == null ) return;
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
+    }
 }
