8daceda8738a389573233984a39aea7001a6267e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-07-24 11:41:44 +0530

    data_structures/trees/InorderTraversal.java

---
+++ data_structures/trees/InorderTraversal.java
@@ -0,0 +1,20 @@
+package data_structures.trees;
+
+import java.util.Stack;
+
+public class InorderTraversal {
+    public void inorderTraversal ( TreeNode node){
+        if ( node == null ) return;
+        Stack<TreeNode> stack = new Stack<>();
+        while ( !stack.isEmpty() || node != null ){
+            if ( node != null ) {
+                stack.push(node);
+                node = node.left;
+            } else {
+                node = stack.pop();
+                System.out.println(node.data);
+                node = node.right;
+            }
+        }
+    }
+}
