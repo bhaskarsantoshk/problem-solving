fa5e1e1cff76d305a5cb8587c04d8ddb9d1cc408
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-07-28 22:03:26 +0530

    data_structures/trees/BST.java

--- data_structures/trees/BST.java
+++ data_structures/trees/BST.java
@@ -1,6 +1,7 @@
 package data_structures.trees;
 
 public class BST {
+    static TreeNode prev = null;
     public static boolean isBst(TreeNode root) {
         return isBst(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
@@ -11,5 +12,22 @@ public class BST {
             return false;
         }
         return isBst(root.left, minValue, root.data) && isBst(root.right, root.data, maxValue);
+    }
+
+    public static boolean isBSTOptimized (TreeNode root){
+        if ( root == null ){
+            return true;
+        }
+        if ( !isBSTOptimized(root.left)){
+            return false;
+        }
+        if ( prev != null && prev.data >= root.data){
+            return false;
+        }
+        prev = root;
+        if ( !isBSTOptimized(root.right)){
+            return false;
+        }
+        return true;
     }
 }
