05e72b1ebd617d558a22fd99a262bba107d1f928
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-07-29 21:59:12 +0530

    data_structures/trees/ConvertBinaryTreeToDoublyLinkedList.java

---
+++ data_structures/trees/ConvertBinaryTreeToDoublyLinkedList.java
@@ -0,0 +1,7 @@
+package data_structures.trees;
+
+public class ConvertBinaryTreeToDoublyLinkedList {
+    public static TreeNode convertTreeToDoublyLinkedList (TreeNode root){
+        TreeNode head = convertTreeToDoublyLinkedList(root)
+    }
+}
