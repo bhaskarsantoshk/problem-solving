eb0d680de86d25f715c8b66b760d2f8015c0d2c2
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-07-30 22:02:41 +0530

    data_structures/trees/ConvertBinaryTreeToDoublyLinkedList.java

--- data_structures/trees/ConvertBinaryTreeToDoublyLinkedList.java
+++ data_structures/trees/ConvertBinaryTreeToDoublyLinkedList.java
@@ -2,6 +2,29 @@ package data_structures.trees;
 
 public class ConvertBinaryTreeToDoublyLinkedList {
     public static TreeNode convertTreeToDoublyLinkedList (TreeNode root){
-        TreeNode head = convertTreeToDoublyLinkedList(root)
+        TreeNode head = convertTreeToDoublyLinkedListRec(root);
+    }
+
+    private static TreeNode convertTreeToDoublyLinkedListRec(TreeNode root) {
+        if ( root == null ) return null;
+        TreeNode list1 = convertTreeToDoublyLinkedListRec(root.left);
+        TreeNode list2 = convertTreeToDoublyLinkedList(root.right);
+        root.left = root.right = root;
+        TreeNode res = concat(list1, root);
+        res = concat(res, list2);
+        return res;
+    }
+
+    private static TreeNode concat(TreeNode list1, TreeNode list2) {
+        if ( list1 == null ) return list2;
+        if ( list2 == null ) return list1;
+        TreeNode tail1 = list1.left;
+        TreeNode tail2 = list2.left;
+        tail1.right = list2;
+        list2.left = tail1;
+
+        list1.left = tail2;
+        tail2.right = list1;
+        return list1;
     }
 }
