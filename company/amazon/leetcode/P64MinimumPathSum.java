6955f73f2e0b486a7d60598ed55d2a90c1920fbc
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-08-22 17:07:10 +0530

    Minimum Path Sum

--- company/amazon/leetcode/P64MinimumPathSum.java
+++ company/amazon/leetcode/P64MinimumPathSum.java
@@ -14,4 +14,26 @@ public class P64MinimumPathSum {
         return grid[row][col]+Math.min (minPathsumRec(grid, row-1, col), minPathsumRec(grid, row, col-1));
     }
 
+    // Approach 2: DP
+    public int minPathSumDP (int[][] grid){
+        if ( grid == null || grid.length == 0) return 0;
+        int height = grid.length;
+        int width = grid[0].length;
+
+        for ( int row = 0; row<height; row++){
+            for ( int col = 0; col <width; col++){
+                if (row == 0 && col == 0){
+                    continue;
+                } else if ( row == 0){
+                    grid[row][col] += grid[row][col-1];
+                } else if ( col == 0){
+                    grid[row][col] += grid[row-1][col];
+                } else {
+                    grid[row][col]+= Math.min(grid[row-1][col], grid[row][col-1]);
+                }
+            }
+        }
+        return grid[height-1][width-1];
+    }
+
 }
