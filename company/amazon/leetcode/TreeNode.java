e93c83530aa554768de8f8169a1cae4aefd1960e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-08-23 09:26:43 +0530

    TreeNode template

---
+++ company/amazon/leetcode/TreeNode.java
@@ -0,0 +1,19 @@
+package company.amazon.leetcode;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(){
+
+    }
+    TreeNode(int val){
+        this.val = val;
+    }
+    TreeNode (int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
