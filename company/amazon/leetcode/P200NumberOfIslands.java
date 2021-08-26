f8a75cabfce726e3fe9b7fd665a43a46654e0018
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2021-08-26 17:31:47 +0530

    Number of Islands

---
+++ company/amazon/leetcode/P200NumberOfIslands.java
@@ -0,0 +1,30 @@
+package company.amazon.leetcode;
+
+public class P200NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        if ( grid == null || grid.length == 0){
+            return 0;
+        }
+        int numIslands = 0;
+        for ( int i=0; i<grid.length; i++){
+            for ( int j=0; j<grid[0].length; j++){
+                if ( grid[i][j] =='1' ){
+                    dfs(grid, i, j);
+                    numIslands++;
+                }
+            }
+        }
+        return numIslands;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if ( i<0|| i>= grid.length || j<0 || j>= grid[0].length || grid[i][j] != '1'){
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i+1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
+    }
+}
