365d7a1b24d8f11e4a9446817937fec32951db9c
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2022-03-17 23:09:36 +0530

    company/amazon/leetcode/P387FirstUniqueCharacterInAString.java

---
+++ company/amazon/leetcode/P387FirstUniqueCharacterInAString.java
@@ -0,0 +1,14 @@
+package company.amazon.leetcode;
+
+public class P387FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        int freq[] = new int[26];
+        for ( int i=0; i<s.length(); i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        for ( int i=0; i<s.length(); i++){
+            if ( freq[s.charAt(i)-'a'] == 1) return i;
+        }
+        return -1;
+    }
+}
