572198f0b4a29e5c781ec05ce5a0e62f12e540db
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2022-03-26 12:28:00 +0530

    company/amazon/leetcode/P239SlidingWindowMaximum.java

--- company/amazon/leetcode/P239SlidingWindowMaximum.java
+++ company/amazon/leetcode/P239SlidingWindowMaximum.java
@@ -11,17 +11,19 @@ public class P239SlidingWindowMaximum {
         }
         int res[] = new int[nums.length-k+1];
         int resIndex = 0;
-        Deque<Integer> dequeue = new ArrayDeque<>();
-        for ( int i=0; i<nums.length; i++) {
-            if ( !dequeue.isEmpty() && dequeue.peek() < i-k+1){
-                dequeue.poll();
+        Deque<Integer> deque = new ArrayDeque<>();
+        for ( int i=0; i<nums.length; i++){
+            // Remove the indexes that are outside window
+            while (!deque.isEmpty() && deque.peek() < i-k+1){
+                deque.poll();
             }
-            while ( !dequeue.isEmpty() && nums[dequeue.peekLast()] < nums[i]){
-                dequeue.poll();
+
+            while (!deque.isEmpty() && nums[deque.pollLast()] < nums[i]){
+                deque.pollLast();
             }
-            dequeue.offer(i);
-            if ( i>= k-1){
-                res[resIndex++] = nums[dequeue.peek()];
+            deque.offer(i);
+            if ( i >= k-1){
+                res[resIndex++] = deque.peek();
             }
         }
         return res;
