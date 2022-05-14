fd0fbf974fb753de13d7a0f89a025fad3c646b2a
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2022-05-14 21:25:02 +0530

    BinaryTreeInorderTraversal.java

---
+++ express_prep/amzn/top_voted/BinaryTreeInorderTraversal.java
@@ -0,0 +1,39 @@
+package express_prep.amzn.top_voted;
+
+import data_structures.stacks_and_queues.Stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if ( root == null ) return result;
+        Stack<TreeNode> stack = new Stack<>();
+        while ( root != null || !stack.isEmpty()) {
+            while ( root != null ){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+        return result;
+    }
+
+    // Recursion
+    public List<Integer> inorderTraversalRec(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder( root, res);
+        return res;
+    }
+
+    private void inorder(TreeNode root, List<Integer> res) {
+        if ( root == null ) return;
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+
+    }
+}
