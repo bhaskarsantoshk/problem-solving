55a5d7f6334ec9e7d5f6cf169ad32168881d69e9
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2022-05-14 21:25:26 +0530

    ListNode.java

---
+++ express_prep/amzn/top_voted/ListNode.java
@@ -0,0 +1,19 @@
+package express_prep.amzn.top_voted;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+
+    }
+
+    ListNode ( int val){
+        this.val = val;
+    }
+
+    ListNode ( int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
