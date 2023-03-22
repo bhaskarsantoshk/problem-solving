c2ccb3ccc8b2189403e0428975f5db107dfcd48a
unknown <bhaskarsantoshk@gmail.com>
2023-03-22 22:57:28 +0530

    starting again

--- dynamic_programming/knapsack/Knapsack.java
+++ dynamic_programming/knapsack/Knapsack.java
@@ -1,5 +1,6 @@
 package dynamic_programming.knapsack;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Knapsack {
@@ -12,5 +13,17 @@ public class Knapsack {
                     getMaximumKnapsackValue(c, weights, values, n-1));
         }
         return getMaximumKnapsackValue(c, weights, values, n-1);
+    }
+
+    public int getMaximumKnapsackValueMemoized ( int c, List<Integer> weights, List<Integer> values, int n ) {
+        int [][] memo = new int[n+1][c+1];
+        for ( int[] row: memo) {
+            Arrays.fill(row, -1);
+        }
+        return getMaximumKnapsackValue(c,weights, values, n, memo);
+    }
+
+    private int getMaximumKnapsackValue(int c, List<Integer> weights, List<Integer> values, int n, int[][] memo) {
+        return 0;
     }
 }
