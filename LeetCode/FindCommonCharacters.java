af16f5db61b01e73f33db909aed6bedfb736083c
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2024-06-05 23:45:18 +0530

    Create FindCommonCharacters.java

---
+++ LeetCode/FindCommonCharacters.java
@@ -0,0 +1,24 @@
+class P1002FindCommonCharacters {
+    public List<String> commonChars(String[] words) {
+        int gc[] = new int[26];
+        Arrays.fill(gc, Integer.MAX_VALUE);
+        for ( String word: words){
+            int []lc = new int[26];
+            for ( char c: word.toCharArray()){
+                lc[c-'a']++;
+            }
+            for ( int i=0; i<26; i++){
+                gc[i] = Math.min(gc[i], lc[i]);
+            }
+        }
+
+        List <String> result = new ArrayList<>();
+        for (char c='a'; c<='z'; c++){
+            while ( gc[c-'a']>0){
+                result.add(String.valueOf(c));
+                gc[c-'a']--;
+            }
+        }
+        return result;
+    }
+}
