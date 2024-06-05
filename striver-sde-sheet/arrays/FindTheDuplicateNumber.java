5cb6711c4b13c539a2a8a28eee85c90971e3cf9d
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2024-06-05 23:26:36 +0530

    Create FindTheDuplicateNumber.java

---
+++ striver-sde-sheet/arrays/FindTheDuplicateNumber.java
@@ -0,0 +1,19 @@
+class FindTheDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        if ( nums == null || nums.length == 0) return -1;
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+
+        while ( slow != fast ){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        slow = 0;
+        while ( slow != fast ){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
