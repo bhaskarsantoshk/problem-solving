6acc9f0e3a930cdb7a3ed30e670f3bba7e4662e3
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2024-06-18 16:32:10 +0530

    Update P826MostProfitAssigningWork.java

--- LeetCode/P826MostProfitAssigningWork.java
+++ LeetCode/P826MostProfitAssigningWork.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.*;
+
 public class P826MostProfitAssigningWork {
 
     // difficulty = [2,4,6,8,10]
@@ -29,6 +31,63 @@ public class P826MostProfitAssigningWork {
                 }
             }
             maxProfit += maxProfitForGivenAbility;
+        }
+        return maxProfit;
+    }
+
+
+    // Optimized - Approach 1
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < difficulty.length; i++) {
+            jobs.add(new Job(difficulty[i], profit[i]));
+        }
+        // Sort jobs by difficulty
+        jobs.sort(Comparator.comparingInt(a -> a.difficulty));
+        // Sort workers by their ability
+        Arrays.sort(worker);
+
+        int maxProfit = 0;
+        int profitForGivenAbility = 0;
+        int index = 0;
+
+        for (int ability : worker) {
+            // Update the max profit for the current ability
+            while (index < jobs.size() && ability >= jobs.get(index).difficulty) {
+                profitForGivenAbility = Math.max(profitForGivenAbility, jobs.get(index).profit);
+                index++;
+            }
+            maxProfit += profitForGivenAbility;
+        }
+        return maxProfit;
+    }
+
+    class Job {
+        int difficulty;
+        int profit;
+
+        public Job(int difficulty, int profit) {
+            this.difficulty = difficulty;
+            this.profit = profit;
+        }
+    }
+
+
+    // Optimized - Approach 2
+    public int maxProfitAssignmentUsingTreeMap(int[] difficulty, int[] profit, int[] worker) {
+        TreeMap<Integer, Integer> job = new TreeMap<>();
+        job.put(0,0);
+        for ( int i=0; i<difficulty.length; i++ ) {
+            job.put(difficulty[i], Math.max(job.getOrDefault(difficulty[i], 0) , profit[i]));
+        }
+        int maxProfitSoFar = 0;
+        for (Integer difficultyKey : job.keySet()) {
+            maxProfitSoFar = Math.max(maxProfitSoFar, job.get(difficultyKey));
+            job.put(difficultyKey, maxProfitSoFar);
+        }
+        int maxProfit = 0;
+        for ( int ability: worker){
+            maxProfit+= job.floorEntry(ability).getValue();
         }
         return maxProfit;
     }
