8adc636bb104839c4c54558ce38662d63696696d
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2024-06-20 22:48:23 +0530

    Update P1552MagneticForceBetweenTwoBalls.java

--- LeetCode/P1552MagneticForceBetweenTwoBalls.java
+++ LeetCode/P1552MagneticForceBetweenTwoBalls.java
@@ -35,6 +35,37 @@ public class P1552MagneticForceBetweenTwoBalls {
         return maxGap;
     }
 
+    public int maxDistanceBinarySearch(int[] position, int m) {
+        Arrays.sort(position);
+        int min = position[0];
+        int max = position[position.length-1];
+        int maxGap = max - min;
+        int start = 1, end = maxGap;
+        int ans = -1;
+        while ( start <= end){
+            int mid = (start + end) / 2;
+            if ( possible(position, m, mid)){
+               start = mid+1;
+               ans = mid;
+            } else {
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean possible ( int[] position, int m, int gap ){
+        int numOfBalls = 1;
+        int index = 0;
+        for ( int i=0; i<position.length; i++){
+            if (position[i] -position[index] >= gap){
+                index = i;
+                numOfBalls++;
+            }
+        }
+        return numOfBalls>= m;
+    }
+
 
     public static void main(String[] args) {
         int [] position = {1,2,3,4,7};
@@ -45,5 +76,14 @@ public class P1552MagneticForceBetweenTwoBalls {
         position = new int[] {5,4,3,2,1,1000000000};
          m = 2;
         System.out.println(obj.maxDistance(position, m));
+
+        position = new int[] {1,2,3,4,7};
+        m = 3;
+        obj = new P1552MagneticForceBetweenTwoBalls();
+        System.out.println(obj.maxDistanceBinarySearch(position, m));
+
+        position = new int[] {5,4,3,2,1,1000000000};
+        m = 2;
+        System.out.println(obj.maxDistanceBinarySearch(position, m));
     }
 }
