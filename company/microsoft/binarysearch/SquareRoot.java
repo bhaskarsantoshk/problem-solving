f78e908d1a022e81175a82913d35905826ea5166
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2024-06-28 12:13:07 +0530

    Create SquareRoot.java

---
+++ company/microsoft/binarysearch/SquareRoot.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int sqrt(int A) {
+        if ( A == 0 ) return 0;
+        int start = 1, end = A;
+        int ans = 0;
+        while ( start <= end ){
+            int mid = start + ( end - start)/2;
+            if ( mid  == A/mid ){
+                return mid;
+            } else if ( mid > A/mid ){
+                end = mid-1;
+            } else {
+                ans = mid;
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+}
