0935940e5e08a1f4fa70a794d05d1066381bab99
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2024-07-03 23:27:09 +0530

    Update P159MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java

--- LeetCode/P159MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
+++ LeetCode/P159MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
@@ -2,6 +2,30 @@ package LeetCode;
 
 import java.util.Arrays;
 
+/*
+A = [1,5,6,13,14,15,16,17]
+n = 8
+
+Case 1: kill 3 biggest elements
+
+All three biggest elements can be replaced with 14
+[1,5,6,13,14,15,16,17] -> [1,5,6,13,14,14,14,14] == can be written as A[n-4] - A[0] == (14-1 = 13)
+
+Case 2: kill 2 biggest elements + 1 smallest elements
+
+[1,5,6,13,14,15,16,17] -> [5,5,6,13,14,15,15,15] == can be written as A[n-3] - A[1] == (15-5 = 10)
+
+Case 3: kill 1 biggest elements + 2 smallest elements
+
+[1,5,6,13,14,15,16,17] -> [6,6,6,13,14,15,16,16] == can be written as A[n-2] - A[2] == (16-6 = 10)
+
+Case 4: kill 3 smallest elements
+
+[1,5,6,13,14,15,16,17] -> [13,13,13,13,14,15,16,17] == can be written as A[n-1] - A[3] == (17-13 = 4)
+
+
+*/
+
 public class P159MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
     public int minDifference(int[] nums) {
         if ( nums == null || nums.length <= 4 ) return 0;
