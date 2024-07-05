67ce3eab7f7c5c9d35d8f0753f5e58baab44405a
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2024-07-06 00:15:51 +0530

    Update P20ValidParentheses.java

--- LeetCode/P20ValidParentheses.java
+++ LeetCode/P20ValidParentheses.java
@@ -4,20 +4,12 @@ import java.util.Stack;
 
 public class P20ValidParentheses {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
-        for(char c: s.toCharArray()){
-            if(c=='('){
-                stack.push(')');
-            }
-            else if(c=='{'){
-                stack.push('}');
-            }
-            else if(c=='['){
-                stack.push(']');
-            }
-            else if(stack.isEmpty() || stack.pop()!= c){
-                return false;
-            }
+        Stack<Character> stack = new Stack<>();
+        for ( char c : s.toCharArray() ){
+            if ( c == '{') stack.push('}');
+            else if ( c == '[') stack.push(']');
+            else if ( c == '(') stack.push(')');
+            else if ( stack.isEmpty() || stack.pop() != c) return false;
         }
         return stack.isEmpty();
     }
