fab118900798c196d8c6d91a6f3a4eee20b50eb4
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2024-07-05 20:12:31 +0530

    Update TrailingZerosinFactorial.java

--- company/microsoft/TrailingZerosinFactorial.java
+++ company/microsoft/TrailingZerosinFactorial.java
@@ -2,12 +2,16 @@ package company.microsoft;
 
 public class TrailingZerosinFactorial {
     public int trailingZeroes(int A) {
-        int fives=0;
-        while(A!=0){
-            A=A/5;
-            if(A!=0) {
-                fives+=A;
-            }
+        // prime factorization
+        // number of 2s and 5s will add up to trailingZeroes
+        // number of 5s are less than number of 2s
+        // number of 5s -> A/5
+        // there will be 25s that also has an extra number of 5s to the above result ,
+        // there will be 125s that also has an extra 5 .. so on
+        int fives = 0;
+        while ( A != 0 ){
+            A = A/5;
+            fives += A;
         }
         return fives;
     }
