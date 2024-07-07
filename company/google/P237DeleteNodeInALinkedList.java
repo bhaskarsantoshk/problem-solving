f98fcded30e38686d138e1a3d262277e7f76bd88
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-07 21:11:52 +0530

    few other variants for deletion in LL

--- company/google/P237DeleteNodeInALinkedList.java
+++ company/google/P237DeleteNodeInALinkedList.java
@@ -12,4 +12,41 @@ public class P237DeleteNodeInALinkedList {
         node.val = node.next.val;
         node.next = node.next.next;
     }
+
+    public ListNode deleteFirst(ListNode head){
+        if ( head == null || head.next == null ) return null;
+        ListNode temp = head;
+        head = head.next;
+        return head;
+    }
+
+    public ListNode deleteLast(ListNode head){
+        if ( head == null || head.next == null ) return null;
+        ListNode temp = head;
+        while ( temp.next.next != null ){
+            temp = temp.next;
+        }
+        temp.next = null;
+        return head;
+    }
+
+    public ListNode deleteKthNode ( ListNode head, int k ){
+        if ( head == null ) return head;
+        if ( k == 1) {
+            head = null;
+            return head;
+        }
+        ListNode cur = head, prev = null;
+        int count = 0;
+        while ( cur != null ){
+            count++;
+            if ( count == k){
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
+        return head;
+    }
 }
