0a396585eb7aa4faf1df496b9324683e333c5b09
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-08 20:56:10 +0530

    recursive + bottom up

--- LeetCode/P1823FindTheWinnerOfTheCircularGame.java
+++ LeetCode/P1823FindTheWinnerOfTheCircularGame.java
@@ -17,4 +17,24 @@ public class P1823FindTheWinnerOfTheCircularGame {
         }
         return queue.poll();
     }
+
+    public int findTheWinnerRec(int n, int k) {
+        return winnerHelper(n, k)+1;
+    }
+
+    private int winnerHelper(int n, int k) {
+        if ( n == 1) return 0;
+        return ( winnerHelper(n-1, k)+k)%n;
+    }
+
+    public int findTheWinnerBottomUp(int n, int k) {
+        int val = 0;
+        for ( int i=2; i<=n; i++){
+            val = (val+k)%i;
+        }
+        return val+1;
+    }
+
+
+
 }
