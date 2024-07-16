46dac754c7a7bef1d87c6cbe90e02f388ab34d7c
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-16 23:14:32 +0530

    company/google/P296StepByStepDirectionsFromABinaryTreeNodeToAnother.java

---
+++ company/google/P296StepByStepDirectionsFromABinaryTreeNodeToAnother.java
@@ -0,0 +1,43 @@
+package company.google;
+
+import company.TreeNode;
+
+public class P296StepByStepDirectionsFromABinaryTreeNodeToAnother {
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        TreeNode lca = lca(root, startValue, destValue);
+        StringBuilder pathToStart = new StringBuilder();
+        StringBuilder pathToDest = new StringBuilder();
+
+        findPath(lca, startValue, pathToStart);
+        findPath(lca, destValue, pathToDest);
+
+        StringBuilder directions = new StringBuilder();
+        directions.append("U".repeat(pathToStart.length()));
+        directions.append(pathToDest);
+        return directions.toString();
+    }
+
+    private boolean findPath(TreeNode node, int target, StringBuilder path) {
+        if ( node == null) return false;
+        if ( node.val == target) return true;
+
+        path.append("L");
+        if ( findPath(node.left, target, path)) return true;
+        path.setLength(path.length()-1);
+
+        path.append("R");
+        if ( findPath(node.right, target, path)) return true;
+        path.setLength(path.length()-1);
+        return false;
+    }
+
+    private TreeNode lca (TreeNode root, int p, int q){
+        if ( root == null ) return root;
+        TreeNode left = lca(root.left, p, q);
+        TreeNode right = lca( root.right, p, q);
+        if ( (left != null && right != null ) || ( root.val == p || root.val == q ) ){
+            return root;
+        }
+        return ( left != null ) ? left : right;
+    }
+}
