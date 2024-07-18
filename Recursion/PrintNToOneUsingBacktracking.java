1f8c3f09bbb49ec2e74b4de2618b8dadb1e47c39
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-18 23:04:26 +0530

    Recursion/PrintNToOneUsingBacktracking.java

---
+++ Recursion/PrintNToOneUsingBacktracking.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PrintNToOneUsingBacktracking {
+    public static void print(int i, int n){
+        if ( i > n) return;
+        print(i+1, n);
+        System.out.println(i);
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        print(1, n);
+    }
+}
