e1deb69fd8e26ae040d68820d69de06556b80b20
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-18 22:48:53 +0530

    Recursion/PrintOneToTen.java

---
+++ Recursion/PrintOneToTen.java
@@ -0,0 +1,9 @@
+package Recursion;
+
+public class PrintOneToTen {
+    public void printNum(int i, int n){
+        if ( i > n) return;
+        System.out.println(i);
+        printNum(i+1, n);
+    }
+}
