4984b0cf605ea589b25b67f6135d2162f7e0e0a1
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-20 16:20:04 +0530

    company/microsoft/linkedlists/ReverseLinkedList.java

--- company/microsoft/linkedlists/ReverseLinkedList.java
+++ company/microsoft/linkedlists/ReverseLinkedList.java
@@ -3,18 +3,6 @@ package company.microsoft.linkedlists;
 import company.microsoft.ListNode;
 
 public class ReverseLinkedList {
-    public ListNode reverseList(ListNode head) {
-        if ( head == null ) return head;
-        ListNode prev = null;
-        ListNode cur = head;
-        while ( cur != null ){
-            ListNode next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
-        }
-        return prev;
-    }
 
     // Bruteforce
     // traverse through LL, put elements in stack
@@ -29,4 +17,34 @@ public class ReverseLinkedList {
     // then move cur to next
     // repeat it
     // T = O(n) S = O(1)
+    public ListNode reverseList(ListNode head) {
+        if ( head == null ) return head;
+        ListNode prev = null;
+        ListNode cur = head;
+        while ( cur != null ){
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    // Recursive
+    // 1 2 3 4 5
+    // reaches 5 -> return 5 to previous func call
+    // head here is 4
+    // next = 5
+    // 5 -> 4 returns 5 to prev func call with 3
+    // head here is 3
+    // next = 4
+    // 5 -> 4 -> 3 etc.
+    public ListNode reverseListRec(ListNode head) {
+        if ( head == null || head.next == null ) return head;
+        ListNode newHead =  reverseList(head.next);
+        ListNode next = head.next;
+        next.next = head;
+        head.next = null;
+        return newHead;
+    }
 }
