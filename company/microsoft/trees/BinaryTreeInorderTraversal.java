61cbf6d4f0ffab188f677e25607ca9c7db43765a
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-22 14:12:18 +0530

    company/microsoft/trees/BinaryTreeInorderTraversal.java

---
+++ company/microsoft/trees/BinaryTreeInorderTraversal.java
@@ -0,0 +1,25 @@
+package company.microsoft.trees;
+
+import company.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
