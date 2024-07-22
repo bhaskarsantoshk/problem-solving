4d5d7e9006e103f305ece86415c0f2a9b7812908
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-22 16:25:02 +0530

    company/microsoft/trees/BinaryTreeZigzagLevelOrderTraversal.java

--- company/microsoft/trees/BinaryTreeZigzagLevelOrderTraversal.java
+++ company/microsoft/trees/BinaryTreeZigzagLevelOrderTraversal.java
@@ -5,6 +5,8 @@ import company.TreeNode;
 import java.util.*;
 
 public class BinaryTreeZigzagLevelOrderTraversal {
+
+    // Using Queue - classic Level Order algo
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if ( root == null ) return res;
@@ -23,6 +25,32 @@ public class BinaryTreeZigzagLevelOrderTraversal {
             }
             if ( !leftToRight ) Collections.reverse(res.get(level));
             level++;
+            leftToRight = !leftToRight;
+        }
+        return res;
+    }
+
+    // Using Deque
+    public List<List<Integer>> zigzagLevelOrderUsingDeque(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if ( root == null ) return res;
+        boolean leftToRight = true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while ( !queue.isEmpty() ){
+            int size = queue.size();
+            Deque<Integer> levelList = new LinkedList<>();
+            for ( int i=0; i<size; i++){
+                TreeNode node = queue.poll();
+                if ( leftToRight ){
+                    levelList.addFirst(node.val);
+                } else {
+                    levelList.addLast(node.val);
+                }
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add( new ArrayList<>(levelList));
             leftToRight = !leftToRight;
         }
         return res;
