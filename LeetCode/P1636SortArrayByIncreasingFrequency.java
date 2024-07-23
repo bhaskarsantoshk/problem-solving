605d41028ecde93ad4c986e422df32ba0530a563
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-23 23:49:40 +0530

    LeetCode/P1636SortArrayByIncreasingFrequency.java

---
+++ LeetCode/P1636SortArrayByIncreasingFrequency.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class P1636SortArrayByIncreasingFrequency {
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> map = new HashMap();
+        Arrays.stream(nums).forEach(n -> map.put(n, map.getOrDefault(n, 0) + 1 ));
+        return Arrays.stream(nums).boxed()
+                .sorted((a,b)->map.get(a) != map.get(b) ? map.get(a)-map.get(b) : b-a)
+                .mapToInt(n->n)
+                .toArray();
+    }
+}
