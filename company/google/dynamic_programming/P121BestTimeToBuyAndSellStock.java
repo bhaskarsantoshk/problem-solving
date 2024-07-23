44eec5e793cc7646817ae37344f34a07657841c5
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-23 14:48:17 +0530

    company/google/dynamic_programming/

---
+++ company/google/dynamic_programming/P121BestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+package company.google.dynamic_programming;
+
+public class P121BestTimeToBuyAndSellStock {
+    // if you are selling on ith day - you buy on the min price from 1st to i-1
+    // track minSoFar
+    // also track profit
+    public int maxProfit(int[] prices) {
+        if ( prices == null || prices.length == 0) return 0;
+        int minSoFar = Integer.MAX_VALUE, maxProfit = 0;
+        for ( int i=0; i<prices.length; i++){
+            minSoFar = Math.min(prices[i], minSoFar);
+            maxProfit = Math.max(maxProfit, prices[i]-minSoFar);
+        }
+        return maxProfit;
+    }
+}
