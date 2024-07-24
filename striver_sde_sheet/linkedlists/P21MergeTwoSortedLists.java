4dd8ed81be6f5aacc732089e1f77e85b23027fe0
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-24 19:17:44 +0530

    striver_sde_sheet/linkedlists/P21MergeTwoSortedLists.java

--- striver_sde_sheet/linkedlists/P21MergeTwoSortedLists.java
+++ striver_sde_sheet/linkedlists/P21MergeTwoSortedLists.java
@@ -50,4 +50,18 @@ public class P21MergeTwoSortedLists {
         if ( list2 != null ) cur.next = list2;
         return dummyNode.next;
     }
+
+
+    public ListNode mergeTwoListsRecursive(ListNode list1, ListNode list2) {
+        if ( list1 == null ) return list2;
+        if ( list2 == null ) return list1;
+
+        if (list1.val <= list2.val){
+            list1.next = mergeTwoListsRecursive(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoListsRecursive(list1, list2.next);
+            return list2;
+        }
+    }
 }
