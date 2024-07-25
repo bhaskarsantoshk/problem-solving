b586b116716c4c1d18edfb0eb33b559e2641e08f
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-25 17:41:30 +0530

    striver_sde_sheet/stacks_and_queues/P496NextGreaterElementI.java

--- striver_sde_sheet/stacks_and_queues/P496NextGreaterElementI.java
+++ striver_sde_sheet/stacks_and_queues/P496NextGreaterElementI.java
@@ -1,5 +1,9 @@
 package striver_sde_sheet.stacks_and_queues;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class P496NextGreaterElementI {
     public int[] nextGreaterElementBruteForce(int[] nums1, int[] nums2) {
         int res[] = new int[nums1.length];
@@ -29,4 +33,27 @@ public class P496NextGreaterElementI {
 
         return res;
     }
+
+    // Use the knowledge of P503NextGreaterElementII
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreater = new HashMap<>(); // map num to next greater
+        Stack<Integer> stack = new Stack<>();
+        for ( int i= nums2.length-1; i>=0 ;i--){
+            while ( !stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
+            }
+            if ( stack.isEmpty() ) {
+                nextGreater.put(nums2[i], -1);
+            } else {
+                nextGreater.put(nums2[i], stack.peek());
+            }
+            stack.push(nums2[i]);
+        }
+        int res[] = new int[nums1.length];
+        for ( int i=0; i<nums1.length; i++){
+            res[i] = nextGreater.get(nums1[i]);
+        }
+        return res;
+    }
+
 }
