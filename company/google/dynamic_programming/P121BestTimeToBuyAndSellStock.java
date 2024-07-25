ffbaf46388e7f28c746bb4009ed5bd6be32d9c3c
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-26 00:01:10 +0530

    company/google/dynamic_programming/P53MaximumSubarray.java

--- company/google/dynamic_programming/P121BestTimeToBuyAndSellStock.java
+++ company/google/dynamic_programming/P121BestTimeToBuyAndSellStock.java
@@ -1,33 +1,6 @@
 package company.google.dynamic_programming;
 
 public class P121BestTimeToBuyAndSellStock {
-    // Brute force
-    public int maxSubArray1(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
-        for ( int i=0; i<nums.length; i++){
-            for ( int j=i; j<nums.length; j++){
-                int sum = 0;
-                for ( int k=i; k<=j; k++){
-                    sum += nums[k];
-                }
-                maxSum = Math.max(sum, maxSum);
-            }
-        }
-        return maxSum;
-    }
-
-    public int maxSubArray2(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
-        for ( int i=0; i<nums.length; i++){
-            int sum = 0;
-            for ( int j=i; j<nums.length; j++){
-                sum += nums[j];
-                maxSum = Math.max(sum, maxSum);
-            }
-        }
-        return maxSum;
-    }
-
 
     // if you are selling on ith day - you buy on the min price from 1st to i-1
     // track minSoFar
--- company/google/dynamic_programming/P53MaximumSubarray.java
+++ company/google/dynamic_programming/P53MaximumSubarray.java
@@ -1,6 +1,34 @@
 package company.google.dynamic_programming;
 
 public class P53MaximumSubarray {
+    
+    // Brute force
+    public int maxSubArray1(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        for ( int i=0; i<nums.length; i++){
+            for ( int j=i; j<nums.length; j++){
+                int sum = 0;
+                for ( int k=i; k<=j; k++){
+                    sum += nums[k];
+                }
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+        return maxSum;
+    }
+
+    public int maxSubArray2(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        for ( int i=0; i<nums.length; i++){
+            int sum = 0;
+            for ( int j=i; j<nums.length; j++){
+                sum += nums[j];
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+        return maxSum;
+    }
+
     public int maxSubArray(int[] nums) {
         int sum = nums[0];
         int max = nums[0];
