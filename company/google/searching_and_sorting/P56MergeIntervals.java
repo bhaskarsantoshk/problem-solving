a50ccf67b5d1467ce01f23b28a06711035ab465d
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-26 21:44:44 +0530

    company/google/searching_and_sorting/

---
+++ company/google/searching_and_sorting/P56MergeIntervals.java
@@ -0,0 +1,26 @@
+package company.google.searching_and_sorting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class P56MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        if ( intervals == null || intervals.length == 0) return intervals;
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0], b[0]));
+        int[] newInterval = intervals[0];
+        List<int[]> res = new ArrayList<>();
+        res.add(newInterval);
+        for ( int[] interval : intervals){
+            // ( 1, 8) ( 2, 10)
+            if ( interval[0] <= newInterval[1]){
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
+            } else {
+                // ( 1, 10 ), ( 15 , 20)
+                res.add(interval);
+                newInterval = interval;
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
