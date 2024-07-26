e49168a3ecdf7147dd159055e985a01e90e4316a
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-26 08:22:52 +0530

    company/google/arrays/P15ThreeSum.java

---
+++ company/google/arrays/P15ThreeSum.java
@@ -0,0 +1,29 @@
+package company.google.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class P15ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if ( nums == null || nums.length < 3) return res;
+        Arrays.sort(nums);
+        for ( int i=0; i<nums.length-2; i++){
+            if ( i>0 && nums[i] == nums[i-1]) continue;
+            int start = i+1, end = nums.length-1;
+            while ( start < end ){
+                int sum = nums[i] + nums[start] + nums[end] ;
+                if ( sum == 0){
+                    res.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                    start++;
+                    end--;
+                    while ( start < end && nums[start] == nums[start-1]) start++;
+                    while ( start < end && nums[end] == nums[end+1]) end--;
+                } else if ( sum < 0) start++;
+                else end--;
+            }
+        }
+        return res;
+    }
+}
