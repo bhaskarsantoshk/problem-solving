b843909ec8d0bd75d69f523c9d1b28b6aaea62b5
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-27 13:15:44 +0530

    company/google/P645SetMismatch.java

--- company/google/P645SetMismatch.java
+++ company/google/P645SetMismatch.java
@@ -45,8 +45,9 @@ public class P645SetMismatch {
     // sum of the squares of array num - sum of the squares of 1st n natural numbers = x^2 - y^2 , we can get x+y
     // sum of the above can get us x
     // and from the x , we can know y
+    // Recommended approach - go for XOR only if you have time and clear explanation
 
-    public int[] findErrorNums(int[] nums) {
+    public int[] findErrorNumsMath(int[] nums) {
         long n = nums.length;
         long sumOfFirstN = ((n) * (n+1))/2;
         long sumOfSquaresOfFirstN = ((n) * (n+1)* (2*n + 1))/6;
@@ -64,4 +65,50 @@ public class P645SetMismatch {
         int missing = (int)(xPlusY-repeat);
         return new int[]{repeat, missing};
     }
+
+
+    // xor of the whole array and xor it with 1..n
+    // this will give the result of xXORy
+    // now find a differentiating bit
+    // and then group the nums and [1..n] using the differentiating bit
+    // since repeating number appears thrice and missing number appears only once
+    // and they definitely belong to different groups
+    // now if we do xor of each group , we will find both missing and repeat
+    public int[] findErrorNumsXOR(int[] nums) {
+        int xXORy = 0;
+        for ( int i=0;i<nums.length; i++){
+            xXORy ^= nums[i];
+            xXORy ^= (i+1);
+        }
+
+        int differentiatingbit = 0;
+        while ( (xXORy & 1<<differentiatingbit) == 0){
+            differentiatingbit++;
+        }
+        int zeroGroup=0, oneGroup=0;
+        for ( int i=0; i <nums.length; i++){
+            if ( (nums[i] & 1 << differentiatingbit) == 0 ) zeroGroup ^= nums[i];
+            else oneGroup ^= nums[i];
+            if ( ((i+1) & (1 << differentiatingbit)) == 0) zeroGroup ^= (i+1);
+            else oneGroup ^= (i+1);
+        }
+
+        int count = 0;
+        for ( int i=0; i<nums.length; i++){
+            if ( zeroGroup == nums[i]){
+                count++;
+            }
+            if ( count == 2) break;
+        }
+        int repeat = -1, missing =-1;
+        if ( count == 2) {
+            repeat = zeroGroup;
+            missing = oneGroup;
+        } else {
+            repeat = oneGroup;
+            missing = zeroGroup;
+        }
+        return new int[]{repeat, missing};
+    }
+
 }
