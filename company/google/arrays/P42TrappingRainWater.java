b96694d91928f46082454c54edfc8f81e47d1dda
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-30 11:48:11 +0530

    company/google/arrays/P42TrappingRainWater.java

--- company/google/arrays/P42TrappingRainWater.java
+++ company/google/arrays/P42TrappingRainWater.java
@@ -24,4 +24,28 @@ public class P42TrappingRainWater {
         }
         return total;
     }
+
+    public int trapSpaceOptimized(int[] height) {
+        int leftMax = 0, rightMax = 0, total = 0;
+        int left = 0, right = height.length-1;
+
+        while ( left < right ){
+            if ( height[left] <= height[right]){
+                if ( leftMax > height[left]){
+                    total += (leftMax-height[left]);
+                } else {
+                    leftMax = height[left];
+                }
+                left++;
+            } else {
+                if ( rightMax > height[right]){
+                    total += ( rightMax-height[right]);
+                } else {
+                    rightMax = height[right];
+                }
+                right--;
+            }
+        }
+        return total;
+    }
 }
