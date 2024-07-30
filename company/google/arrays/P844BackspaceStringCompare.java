0ce5010e1d69f261207642ee332c89cb950a086e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-30 21:44:34 +0530

    company/google/arrays/P844BackspaceStringCompare.java

--- company/google/arrays/P844BackspaceStringCompare.java
+++ company/google/arrays/P844BackspaceStringCompare.java
@@ -1,7 +1,7 @@
 package company.google.arrays;
 
 public class P844BackspaceStringCompare {
-    public boolean backspaceCompare(String s, String t) {
+    public boolean backspaceCompareWithSpace(String s, String t) {
         StringBuilder sb1 = new StringBuilder();
         for ( char c: s.toCharArray()){
             if ( c == '#'){
@@ -19,5 +19,33 @@ public class P844BackspaceStringCompare {
             }
         }
         return sb1.toString().equals(sb2.toString());
+    }
+
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length()-1;
+        int j = t.length()-1;
+        int skipS = 0, skipT = 0;
+
+        while ( i >= 0 || j >= 0){
+            while ( i >=0 ){ // Find position of next possible char in build(S)
+                if ( s.charAt(i)=='#') { i--; skipS++;}
+                else if ( skipS > 0) { i--; skipS--;}
+                else break;
+            }
+
+            while ( j >= 0){ // Find position of next possible char in build(t)
+                if ( t.charAt(j) == '#'){ j--; skipT++;}
+                else if ( skipT > 0) {j--; skipT--;}
+                else break;
+            }
+            // If two actual characters are different
+            if ( i>= 0 && j>=0){
+                if ( s.charAt(i) != t.charAt(j)) return false;
+            }
+            // If expecting to compare char vs nothing
+            if ( (i>=0) != (j>=0)) return false;
+            i--;j--;
+        }
+        return true;
     }
 }
