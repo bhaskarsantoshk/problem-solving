ed6de0a625ba13d087ccc722e870195a0bdfa94e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-07-31 20:37:13 +0530

    company/google/trees_and_graphs/P222CountCompleteTreeNodes.java

--- company/google/trees_and_graphs/P222CountCompleteTreeNodes.java
+++ company/google/trees_and_graphs/P222CountCompleteTreeNodes.java
@@ -7,4 +7,30 @@ public class P222CountCompleteTreeNodes {
         if ( root == null ) return 0;
         return 1+ countNodes(root.left) + countNodes(root.right);
     }
+
+    public int countNodesOptimized(TreeNode root) {
+        if ( root == null ) return 0;
+        int leftDepth = leftDepth(root);
+        int rightDepth = rightDepth(root);
+        if ( leftDepth == rightDepth ) return (1 << leftDepth) -1;
+        return 1 + countNodesOptimized(root.left) + countNodesOptimized(root.right);
+    }
+
+    private int rightDepth(TreeNode root) {
+        int depth = 0;
+        while ( root != null ){
+            root = root.right;
+            depth++;
+        }
+        return depth;
+    }
+
+    private int leftDepth(TreeNode root) {
+        int depth = 0;
+        while ( root != null ){
+            root = root.left;
+            depth++;
+        }
+        return depth;
+    }
 }
