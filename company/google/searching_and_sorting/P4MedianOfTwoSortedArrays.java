7123a04fcf878056fe08e657a165adeadfce668d
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-01 14:50:45 +0530

    company/google/searching_and_sorting/P4MedianOfTwoSortedArrays.java

--- company/google/searching_and_sorting/P4MedianOfTwoSortedArrays.java
+++ company/google/searching_and_sorting/P4MedianOfTwoSortedArrays.java
@@ -23,4 +23,38 @@ public class P4MedianOfTwoSortedArrays {
             return (double) ( merged[merged.length/2] + merged[merged.length/2 + 1])/2;
         }
     }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if ( nums1.length > nums2.length){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int x = nums1.length;
+        int y = nums2.length;
+
+        int start = 0, end = x;
+        while ( start <= end ){
+            int partitionX = (start+end)/2;
+            int partitionY = ( x+y+1)/2 - partitionX;
+
+            int maxLeftX = ( partitionX == 0) ?Integer.MIN_VALUE : nums1[partitionX-1];
+            int minRightX = ( partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxLeftY = ( partitionY == 0) ? Integer.MIN_VALUE: nums2[partitionY-1];
+            int minRightY = ( partitionY == y) ? Integer.MAX_VALUE: nums2[partitionY];
+
+            if ( maxLeftX <= minRightY && maxLeftY <= minRightX){
+                if ((x+y)%2 == 0 ){
+                    return (Math.max(maxLeftX , maxLeftY) + Math.min( minRightX, minRightY))/2.0;
+                }  else {
+                    return Math.max(maxLeftX, maxLeftY);
+                }
+            } else if ( maxLeftX > minRightY){
+                end = partitionX-1;
+            } else{
+                start = partitionX+1;
+            }
+        }
+        return -1;
+    }
 }
