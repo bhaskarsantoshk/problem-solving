bb8f3373d3ea9cd00b2d61eba8a8cd0fd030fa04
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-07 23:37:00 +0530

    striver_sde_sheet/dynamic_programming/P273IntegerToEnglishWords.java

---
+++ striver_sde_sheet/dynamic_programming/P273IntegerToEnglishWords.java
@@ -0,0 +1,21 @@
+package striver_sde_sheet.dynamic_programming;
+
+public class P273IntegerToEnglishWords {
+    String[] tens = {"", "Ten","Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+    public String numberToWords(int num) {
+        if ( num <= 0) return "Zero";
+        return helper(num);
+    }
+
+    private String helper(int num) {
+        if ( num >= 1000000000) return (helper(num/1000000000)+" Billion "+helper(num%1000000000)).trim();
+        if ( num >= 1000000) return (helper(num/1000000)+" Million "+helper(num%1000000)).trim();
+        if ( num >= 1000) return (helper(num/1000)+" Thousand "+helper(num%1000)).trim();
+        if ( num >= 100 ) return (helper(num/100)+" Hundred "+helper(num%100)).trim();
+        if ( num >= 20) return (tens[num/10]+" "+ones[num%10]).trim();
+        return ones[num];
+
+    }
+}
