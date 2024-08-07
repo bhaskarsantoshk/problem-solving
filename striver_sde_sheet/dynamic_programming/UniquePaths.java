f722be46486765e3137bfafea9562109228777ee
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-07 19:32:58 +0530

    striver_sde_sheet/dynamic_programming/UniquePaths.java

--- striver_sde_sheet/dynamic_programming/UniquePaths.java
+++ striver_sde_sheet/dynamic_programming/UniquePaths.java
@@ -48,18 +48,22 @@ public class UniquePaths {
     }
 
     public static int uniquePathsTabularSpaceOptimized(int m, int n) {
-        int[][] dp = new int[m][n];
-        dp[0][0] = 1;
+        int[] prev = new int[n];
+        prev[0] = 1;
         for ( int i=0; i<m; i++){
+            int [] current = new int[n];
             for ( int j=0; j<n; j++){
-                if ( i==0 && j == 0) continue;
-                int up = 0, left = 0;
-                if (i >0) up = dp[i-1][j];
-                if ( j>0) left = dp[i][j-1];
-                dp[i][j] = up + left;
+                if ( i==0 && j == 0) current[j] = 1;
+                else {
+                    int up = 0, left = 0;
+                    if (i > 0) up = prev[j];
+                    if (j > 0) left = current[j - 1];
+                    current[j] = up + left;
+                }
             }
+            prev = current;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 
 
