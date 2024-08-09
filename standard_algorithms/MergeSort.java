6c6093964c0203dfd7cc91387c7a2dcdec988428
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-09 09:29:44 +0530

    standard_algorithms/MergeSort.java

---
+++ standard_algorithms/MergeSort.java
@@ -0,0 +1,90 @@
+package standard_algorithms;
+
+public class MergeSort {
+
+    // Main function that sorts an array using merge sort
+    public static void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            // Recursively sort the two halves
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+
+            // Merge the two halves
+            merge(array, left, mid, right);
+        }
+    }
+
+    // Function to merge two halves of the array
+    public static void merge(int[] array, int left, int mid, int right) {
+        // Find sizes of two subarrays to be merged
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Create temporary arrays
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = array[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            rightArray[j] = array[mid + 1 + j];
+        }
+
+        // Merge the temporary arrays
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of leftArray[] if any
+        while (i < n1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of rightArray[] if any
+        while (j < n2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Function to print the array
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method to test the merge sort
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6, 7};
+        System.out.println("Given Array");
+        printArray(array);
+
+        mergeSort(array, 0, array.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(array);
+    }
+}
+
