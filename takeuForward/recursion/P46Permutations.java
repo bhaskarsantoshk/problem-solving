3c0b39ae1aa631e5cf80af033a37208980189005
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-12 14:03:15 +0530

    takeuForward/recursion/P46Permutations.java

--- takeuForward/recursion/P46Permutations.java
+++ takeuForward/recursion/P46Permutations.java
@@ -26,4 +26,33 @@ public class P46Permutations {
             }
         }
     }
+
+    public List<List<Integer>> permuteWithoutExtraSpace(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        permuteWithoutExtraSpace(0, nums, res);
+        return res;
+    }
+
+    private void permuteWithoutExtraSpace(int index, int[] nums, List<List<Integer>> res) {
+       if ( index == nums.length){
+           List<Integer> list = new ArrayList<>();
+           for ( int i=0; i<nums.length; i++){
+               list.add(nums[i]);
+           }
+           res.add(new ArrayList<>(list));
+           return;
+       }
+
+       for ( int i=index; i<nums.length; i++){
+           swap(nums, i, index);
+           permuteWithoutExtraSpace(index+1, nums, res);
+           swap(nums, i, index );
+       }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
