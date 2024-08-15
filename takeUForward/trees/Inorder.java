e2b589a815940f14054292997d18f1271b504944
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-15 17:43:55 +0530

    takeUForward/trees/Inorder.java

--- takeUForward/trees/Inorder.java
+++ takeUForward/trees/Inorder.java
@@ -1,4 +1,5 @@
 package takeUForward.trees;
+import java.util.Stack;
 
 public class Inorder {
     public void inOrder(TreeNode root){
@@ -6,5 +7,29 @@ public class Inorder {
         inOrder(root.left);
         System.out.println(root.val);
         inOrder(root.right);
+    }
+
+    /*
+              1
+           2     3
+        4     5
+            6    7
+
+          4 2 6 5 7 1 3
+     */
+
+    public void inOrderIterative(TreeNode root){
+        if ( root == null ) return;
+        TreeNode node = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while ( !stack.isEmpty() || node != null){
+            while ( node != null ) {
+                stack.push(root);
+                node = node.left;
+            }
+            node= stack.pop();
+            System.out.println(node.val);
+            node = node.right;
+        }
     }
 }
