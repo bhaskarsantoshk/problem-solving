d76ee91ef52b2e76b3c363ceeb8f9fd6db933b19
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-16 17:37:04 +0530

    takeUForward/trees/P100SameTree.java

---
+++ takeUForward/trees/P100SameTree.java
@@ -0,0 +1,8 @@
+package takeUForward.trees;
+
+public class P100SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if ( p == null || q == null ) return p == null && q == null;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
