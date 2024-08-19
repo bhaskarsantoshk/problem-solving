48b225393ff4805a181ed4c9142398731ec0f8f7
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-19 21:27:16 +0530

    takeUForward/trees/P236LowestCommonAncestorOfABinaryTree.java

---
+++ takeUForward/trees/P236LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,12 @@
+package takeUForward.trees;
+
+public class P236LowestCommonAncestorOfABinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if ( root == null) return null;
+        if ( root.val == p.val || root.val == q.val) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if ( left != null & right != null) return root;
+        return left != null ? left: right;
+    }
+}
