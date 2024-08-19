cc2c53dd66e01f08932552a559a15097ea1f9cef
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-19 23:11:27 +0530

    company/microsoft/ReverseWordsInAString.java

--- company/microsoft/ReverseWordsInAString.java
+++ company/microsoft/ReverseWordsInAString.java
@@ -1,7 +1,26 @@
 package company.microsoft;
 
+import java.util.Stack;
+
 public class ReverseWordsInAString {
     public String reverseWords(String s) {
-        // TODO
+        StringBuilder sb = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+        for ( int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if ( c == ' '){
+                if ( !sb.isEmpty()) stack.push(sb.toString());
+                sb = new StringBuilder();
+            } else {
+                sb.append(c);
+            }
+        }
+        if ( !sb.isEmpty()) stack.push(sb.toString());
+        StringBuilder res = new StringBuilder();
+        while (!stack.isEmpty()){
+            res.append(stack.pop()).append(" ");
+        }
+        res.deleteCharAt(res.length()-1);
+        return res.toString();
     }
 }
