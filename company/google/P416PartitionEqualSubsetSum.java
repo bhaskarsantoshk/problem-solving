84181e1727034fad33ab14b70af2f11137d178d7
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-23 22:48:44 +0530

    company/google/P416PartitionEqualSubsetSum.java

--- company/google/P416PartitionEqualSubsetSum.java
+++ company/google/P416PartitionEqualSubsetSum.java
@@ -18,7 +18,7 @@ public class P416PartitionEqualSubsetSum {
     }
 
     private boolean isSubSetSumPossible(int[] nums, int index, int sum, Boolean[][] memo) {
-        if ( sum == 0) return true;
+        if ( sum == 0) return true; // also covers index = 0 and nums[i]= target scenario
         if ( index < 0 ) return false;
         if (memo[index][sum] != null) return memo[index][sum];
         if ( nums[index] <= sum ){
@@ -28,5 +28,23 @@ public class P416PartitionEqualSubsetSum {
         boolean notPick = isSubSetSumPossible(nums, index-1, sum, memo);
         if ( notPick) return memo[index][sum]= true;
         return memo[index][sum]= false;
+    }
+
+    private boolean isSubSetSumPossibleTabular(int[] nums, int k) {
+        boolean [][] dp = new boolean[nums.length][k+1];
+        for ( int i=0; i<nums.length; i++){
+            dp[i][0] = true;
+        }
+        if ( nums[0] <= k) dp[0][nums[0]] = true;
+
+        for ( int i=1; i<nums.length; i++){
+            for ( int target=1; target<=k; target++){
+                boolean notTake = dp[i-1][target];
+                boolean take= false;
+                if ( nums[i] <= target) take = dp[i-1][target-nums[i]];
+                dp[i][target] = take || notTake;
+            }
+        }
+        return dp[nums.length-1][k];
     }
 }
