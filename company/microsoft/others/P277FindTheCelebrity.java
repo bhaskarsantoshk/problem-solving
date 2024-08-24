31ef0721e6e0fcd33c634ae301c69589a4e05d3e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-24 11:24:31 +0530

    company/microsoft/others/P277FindTheCelebrity.java

---
+++ company/microsoft/others/P277FindTheCelebrity.java
@@ -0,0 +1,27 @@
+package company.microsoft.others;
+
+public class P277FindTheCelebrity {
+    public int findCelebrity(int n) {
+        int[] indegree = new int[n];
+        int[] outdegree = new int[n];
+        for ( int i=0; i<n; i++){
+            for ( int j=0; j<n; j++){
+                if ( i != j && knows(i, j)){
+                    indegree[j]++;
+                }
+                if ( i != j && knows(j, i)){
+                    outdegree[j]++;
+                }
+            }
+        }
+        for ( int i=0; i<n; i++){
+            if ( indegree[i] == n-1 && outdegree[i] == 0) return i;
+        }
+        return -1;
+    }
+
+    private boolean knows(int i, int j) {
+        // Just a mock, actual impl is within the Leetcode class
+        return true;
+    }
+}
