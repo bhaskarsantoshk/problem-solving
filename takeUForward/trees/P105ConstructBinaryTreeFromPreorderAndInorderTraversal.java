c8888d015f5e0c4df9f2387bb6df682de5d36548
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-27 17:08:50 +0530

    takeUForward/trees/P105ConstructBinaryTreeFromPreorderAndInorderTraversal.java

--- takeUForward/trees/P105ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ takeUForward/trees/P105ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -1,5 +1,8 @@
 package takeUForward.trees;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P105ConstructBinaryTreeFromPreorderAndInorderTraversal {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return buildTree(preorder, 0,preorder.length-1, inorder, 0, inorder.length-1);
@@ -17,6 +20,23 @@ public class P105ConstructBinaryTreeFromPreorderAndInorderTraversal {
         }
         root.left = buildTree(preorder, preStart+1, preEnd,inorder, inStart, index-1);
         root.right = buildTree(preorder, preStart+index+1-inStart, preEnd, inorder, index+1, inEnd );
+        return root;
+    }
+
+    public TreeNode buildTreeOptimized(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for ( int i=0; i<inorder.length; i++){
+            map.put(inorder[i], i);
+        }
+        return buildTreeOptimized(preorder, 0,preorder.length-1, inorder, 0, inorder.length-1, map);
+    }
+
+    private TreeNode buildTreeOptimized(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inOrderMap) {
+        if ( inStart > inEnd || preStart > preEnd) return null;
+        TreeNode root = new TreeNode (preorder[preStart]);
+        int index = inOrderMap.get(root.val);
+        root.left = buildTreeOptimized(preorder, preStart+1, preEnd,inorder, inStart, index-1, inOrderMap);
+        root.right = buildTreeOptimized(preorder, preStart+index+1-inStart, preEnd, inorder, index+1, inEnd, inOrderMap);
         return root;
     }
 }
