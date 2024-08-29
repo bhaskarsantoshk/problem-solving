527d3c77388d7fbeaefce82928c99119364a07ba
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-08-29 12:38:28 +0530

    takeUForward/graph/EventualSafeStatesWithTopologicalSort.java

---
+++ takeUForward/graph/EventualSafeStatesWithTopologicalSort.java
@@ -0,0 +1,38 @@
+package takeUForward.graph;
+
+import java.util.*;
+
+public class EventualSafeStatesWithTopologicalSort {
+    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
+        int [] indegree = new int[V];
+        List<List<Integer>> adjRev = new ArrayList<>();
+        for ( int i=0; i<V; i++) adjRev.add(new ArrayList<>());
+        for ( int i=0; i<V; i++){
+            for ( int adjNode: adj.get(i)){
+                // i -> adjNode
+                // need to change from adjNode -> i
+                adjRev.get(adjNode).add(i);
+                indegree[i]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> res = new ArrayList<>();
+        for ( int i=0; i<V; i++){
+            if ( indegree[i] == 0) {
+                q.offer(i);
+                res.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for ( int adjNode: adjRev.get(node)){
+                if ( --indegree[adjNode] == 0) {
+                    res.add(adjNode);
+                    q.offer(adjNode);
+                }
+            }
+        }
+        Collections.sort(res);
+        return res;
+    }
+}
