b6541ca3e79784cdf309e2a38fa31b5cd0907c75
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-09-01 17:00:34 +0530

    takeUForward/dynamicProgramming/DP40BuyAndSellStocksWithTransactionFeeRecursionToSpaceOptimisation.java

--- takeUForward/dynamicProgramming/DP40BuyAndSellStocksWithTransactionFeeRecursionToSpaceOptimisation.java
+++ takeUForward/dynamicProgramming/DP40BuyAndSellStocksWithTransactionFeeRecursionToSpaceOptimisation.java
@@ -2,6 +2,23 @@ package takeUForward.dynamicProgramming;
 
 public class DP40BuyAndSellStocksWithTransactionFeeRecursionToSpaceOptimisation {
     public static int maximumProfit(int[] prices, int n, int fee) {
-
+        int[][] dp = new int[n+1][2];
+        dp[n][0] = dp[n][1] = 0;
+        for ( int index=n-1; index>=0; index--){
+            for ( int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if ( buy == 1){
+                    int take = -prices[index] + dp[index+1][0];
+                    int notTake = dp[index+1][1];
+                    profit = Math.max(take, notTake);
+                } else {
+                    int sell = prices[index] + dp[index+1][1]-fee;
+                    int notSell = dp[index+1][0];
+                    profit = Math.max(sell, notSell);
+                }
+                dp[index][buy]  = profit;
+            }
+        }
+        return dp[0][1];
     }
 }
