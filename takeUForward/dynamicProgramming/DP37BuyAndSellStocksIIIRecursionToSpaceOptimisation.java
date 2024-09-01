62494ef25d76facd8a62ee44b14a303d91379ad2
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-09-01 17:00:05 +0530

    takeUForward/dynamicProgramming/DP37BuyAndSellStocksIIIRecursionToSpaceOptimisation.java

--- takeUForward/dynamicProgramming/DP37BuyAndSellStocksIIIRecursionToSpaceOptimisation.java
+++ takeUForward/dynamicProgramming/DP37BuyAndSellStocksIIIRecursionToSpaceOptimisation.java
@@ -68,4 +68,28 @@ public class DP37BuyAndSellStocksIIIRecursionToSpaceOptimisation {
         }
         return dp[0][1][2];
     }
+
+    public static int maxProfitTabulationSpaceOptimzation(int[] prices) {
+        int n = prices.length;
+        int [][]after = new int[2][3];
+        int [][]curr = new int[2][3];
+        for ( int index=n-1; index>=0; index--){
+            for ( int buy=0; buy<=1; buy++){
+                for ( int cap=1; cap<=2; cap++){
+                    int profit = 0;
+                    if ( buy == 1){
+                        int take = -prices[index] + after[0][cap];
+                        int notTake = after[1][cap];
+                        curr[buy][cap] = Math.max(take, notTake);
+                    } else {
+                        int sell = prices[index] + after[1][cap-1];
+                        int wait = after[0][cap];
+                        curr[buy][cap] = Math.max(sell, wait);
+                    }
+                }
+            }
+            after = (int[][]) curr.clone();
+        }
+        return curr[1][2];
+    }
 }
