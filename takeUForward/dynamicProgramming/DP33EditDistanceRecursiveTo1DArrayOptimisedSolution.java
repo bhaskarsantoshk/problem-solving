a56077c3a7ee6cea045cf32815cac4df6a014840
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-09-01 10:41:01 +0530

    takeUForward/dynamicProgramming/DP33EditDistanceRecursiveTo1DArrayOptimisedSolution.java

--- takeUForward/dynamicProgramming/DP33EditDistanceRecursiveTo1DArrayOptimisedSolution.java
+++ takeUForward/dynamicProgramming/DP33EditDistanceRecursiveTo1DArrayOptimisedSolution.java
@@ -41,4 +41,52 @@ public class DP33EditDistanceRecursiveTo1DArrayOptimisedSolution {
             return memo[i][j] = Math.min(Math.min(insert, delete), replace);
         }
     }
+
+    public static int editDistanceTabular(String str1, String str2) {
+        int n = str1.length(), m = str2.length();
+        int [][] dp = new int[n+1][m+1];
+        for ( int i=0; i<=n; i++){
+            dp[i][0] = i;
+        }
+        for ( int j=0; j<=m; j++){
+            dp[0][j] = j;
+        }
+        for ( int i=1; i<=n; i++){
+            for ( int j=1; j<=m; j++){
+                if (str1.charAt(i-1) == str2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+                else {
+                    int insert = 1 + dp[i][j-1];
+                    int delete = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+                    dp[i][j] = Math.min(insert, delete);
+                    dp[i][j] = Math.min(dp[i][j], replace);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static int editDistanceTabularSpaceOptimized(String str1, String str2) {
+        int n = str1.length(), m = str2.length();
+        int [] prev = new int[m+1];
+        int [] cur = new int[m+1];
+        for ( int j=0; j<=m; j++){
+            prev[j] = j;
+        }
+        for ( int i=1; i<=n; i++){
+            cur[0] = i;
+            for ( int j=1; j<=m; j++){
+                if (str1.charAt(i-1) == str2.charAt(j-1)) cur[j] = prev[j-1];
+                else {
+                    int insert = 1 + cur[j-1];
+                    int delete = 1 + prev[j];
+                    int replace = 1 + prev[j-1];
+                    cur[j] = Math.min(insert, delete);
+                    cur[j] = Math.min(cur[j], replace);
+                }
+            }
+            prev = (int[])cur.clone();
+        }
+        return prev[m];
+    }
 }
