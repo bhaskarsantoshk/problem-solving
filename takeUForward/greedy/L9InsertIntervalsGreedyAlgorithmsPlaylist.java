180cdd30b495ee2fb8714b3ac4a961298e883db8
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-09-05 12:31:34 +0530

    takeUForward/greedy/L9InsertIntervalsGreedyAlgorithmsPlaylist.java

---
+++ takeUForward/greedy/L9InsertIntervalsGreedyAlgorithmsPlaylist.java
@@ -0,0 +1,27 @@
+package takeUForward.greedy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L9InsertIntervalsGreedyAlgorithmsPlaylist {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new ArrayList<>();
+        for ( int[] interval: intervals){
+            // interval.end < newInterval.start
+            if ( newInterval == null || interval[1] < newInterval[0]){
+                res.add(interval);
+            } else if ( interval[0] > newInterval[1]){
+                res.add(newInterval);
+                res.add(interval);
+                newInterval = null;
+            } else {
+                newInterval[0] = Math.min(interval[0], newInterval[0]);
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
+            }
+        }
+        if ( newInterval != null ){
+            res.add(newInterval);
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
