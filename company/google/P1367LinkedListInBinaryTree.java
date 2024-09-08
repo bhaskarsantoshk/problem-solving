e0a84f109436857a70850c470d22da40c4527140
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-09-08 13:01:44 +0530

    company/google/P1367LinkedListInBinaryTree.java

---
+++ company/google/P1367LinkedListInBinaryTree.java
@@ -0,0 +1,19 @@
+package company.google;
+
+import company.TreeNode;
+import company.microsoft.ListNode;
+
+public class P1367LinkedListInBinaryTree {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if ( head == null ) return true;
+        if ( root == null) return false;
+
+        return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    private boolean dfs(ListNode head, TreeNode root) {
+        if ( head == null ) return true;
+        if ( root == null) return false;
+        return head.val == root.val && ( dfs(head.next, root.left)|| dfs(head.next, root.right));
+    }
+}
