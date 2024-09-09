80d50ae3815edaa51002a3f2bddf75e28d201783
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-09-09 23:59:12 +0530

    company/google/P692TopKFrequentWords.java

--- company/google/P692TopKFrequentWords.java
+++ company/google/P692TopKFrequentWords.java
@@ -4,17 +4,44 @@ import java.util.*;
 
 public class P692TopKFrequentWords {
     public List<String> topKFrequent(String[] words, int k) {
-        Map<String, Integer> frequency = new HashMap<>();
-        for ( String word: words) frequency.put(word, frequency.getOrDefault(word,0)+1);
-        PriorityQueue<String> pq = new PriorityQueue<>((s1, s2)->
-                (frequency.get(s1) == frequency.get(s2)) ? s1.compareTo(s2) : frequency.get(s2)-frequency.get(s1));
-        for ( String word: frequency.keySet()){
+        Map<String, Integer> map = new HashMap<>();
+        for ( String word: words){
+            map.put(word, map.getOrDefault(word, 0)+1);
+        }
+        PriorityQueue<String> pq = new PriorityQueue<>((a,b)->
+                map.get(a)== map.get(b) ? a.compareTo(b) : map.get(b)-map.get(a));
+
+        for ( String word: map.keySet()){
             pq.offer(word);
         }
+
         List<String> res = new ArrayList<>();
+
         while ( k-- != 0){
             res.add(pq.poll());
         }
+        return res;
+    }
+
+    public List<String> topKFrequentMemoryOptimized(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for ( String word: words){
+            map.put(word, map.getOrDefault(word, 0)+1);
+        }
+        PriorityQueue<String> pq = new PriorityQueue<>((a,b)->
+                map.get(a)== map.get(b) ? b.compareTo(a) : map.get(a)-map.get(b));
+
+        for ( String word: map.keySet()){
+            pq.offer(word);
+            if ( pq.size() > k) pq.poll();
+        }
+
+        List<String> res = new ArrayList<>();
+
+        while ( k-- != 0){
+            res.add(pq.poll());
+        }
+        Collections.reverse(res);
         return res;
     }
 }
