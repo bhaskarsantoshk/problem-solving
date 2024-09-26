23aa683bb6a57fecc7e34e7cee9062a8c5eafe76
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-09-26 23:58:27 +0530

    LeetCode/P729MyCalendarI.java

---
+++ LeetCode/P729MyCalendarI.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+import java.util.List;
+
+public class P729MyCalendarI {
+    private List<int[]> books = new ArrayList<>();
+    public boolean book(int start, int end) {
+        for (int[] b : books)
+            if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
+        books.add(new int[]{ start, end });
+        return true;
+    }
+}
