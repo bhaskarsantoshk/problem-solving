a47f7cf4e775bfea22301ce154165e155d63935e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-12-27 00:05:39 +0530

    LeetCode/P494TargetSum.java

---
+++ LeetCode/P494TargetSum.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P494TargetSum {
+    public int findTargetSumWays(int[] nums, int s) {
+        Map<Integer, Integer> dp = new HashMap();
+        dp.put(0, 1);
+        for (int num : nums) {
+            Map<Integer, Integer> dp2 = new HashMap();
+            for (int tempSum : dp.keySet()) {
+                int key1 = tempSum + num;
+                dp2.put(key1, dp2.getOrDefault(key1, 0) + dp.get(tempSum));
+                int key2 = tempSum - num;
+                dp2.put(key2, dp2.getOrDefault(key2, 0) + dp.get(tempSum));
+            }
+            dp = dp2;
+        }
+        return dp.getOrDefault(s, 0);
+    }
+}
