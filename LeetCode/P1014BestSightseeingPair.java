875ed1023d6a172b984a46bac299e2b2973c4535
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2024-12-27 21:49:25 +0530

    LeetCode/P1014BestSightseeingPair.java

---
+++ LeetCode/P1014BestSightseeingPair.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class P1014BestSightseeingPair {
+    public int maxScoreSightseeingPair(int[] A) {
+        int res = 0, cur = 0;
+        for (int a: A) {
+            res = Math.max(res, cur + a);
+            cur = Math.max(cur, a) - 1;
+        }
+        return res;
+    }
+}
