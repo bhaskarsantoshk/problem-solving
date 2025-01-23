2dd108ce6bdcfddeb30fb98ecfb21a108f848172
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-01-23 23:11:26 +0530

    takeUForward/binarySearch/BinarySearch.java recusrion

--- takeUForward/binarySearch/BinarySearch.java
+++ takeUForward/binarySearch/BinarySearch.java
@@ -11,4 +11,16 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    public static int searchRec(int[] nums, int target){
+        return search(nums, 0, nums.length-1, target);
+    }
+
+    private static int search(int[] nums, int low, int high, int target) {
+        if ( low > high) return -1;
+        int mid = (low + high)/2;
+        if ( nums[mid] == target) return mid;
+        else if ( nums[mid] > target) return search(nums, low, mid-1, target);
+        else return search(nums, mid+1, high, target);
+    }
 }
