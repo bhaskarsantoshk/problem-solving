5e87143bab74a0486c933e56a63d52c66a663874
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-01-26 09:50:49 +0530

    takeUForward/binarySearch/FirstAndLastPositionOfAnElementInSortedArray.java

--- takeUForward/binarySearch/FirstAndLastPositionOfAnElementInSortedArray.java
+++ takeUForward/binarySearch/FirstAndLastPositionOfAnElementInSortedArray.java
@@ -37,4 +37,45 @@ public class FirstAndLastPositionOfAnElementInSortedArray {
         }
         return ans;
     }
+
+    public static int[] firstAndLastPositionUsingPlainBinarySearch(ArrayList<Integer> arr, int n, int k) {
+        int first = getFirst(arr, n, k);
+        int last = getLast(arr, n, k);
+        if ( first == -1 ) return new int[] {-1,-1};
+        return new int[] {first, last};
+    }
+
+    private static int getLast(ArrayList<Integer> arr, int n, int k) {
+        int low = 0, high = n-1;
+        int last = -1;
+        while ( low <= high){
+            int mid = low + (high -low)/2;
+            if ( arr.get(mid) == k){
+                last = mid;
+                low = mid+1;
+            } else if ( arr.get(mid) < k){
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+        return last;
+    }
+
+    private static int getFirst(ArrayList<Integer> arr, int n, int k) {
+        int low = 0, high = n-1;
+        int first = -1;
+        while ( low <= high){
+            int mid = low + (high-low)/2;
+            if ( arr.get(mid) == k){
+                first = mid;
+                high = mid-1;
+            } else if ( arr.get(mid) < k){
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+        return first;
+    }
 }
