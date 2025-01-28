302d7bbaf562766f76b8bdbd2287d6661fd96359
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-01-28 18:01:33 +0530

    takeUForward/binarySearch/FindNthRootOfM.java

--- takeUForward/binarySearch/FindNthRootOfM.java
+++ takeUForward/binarySearch/FindNthRootOfM.java
@@ -8,14 +8,27 @@ public class FindNthRootOfM {
         while ( low <= high ){
             int mid = low + (high-low)/2;
 
-            if ( power(mid, n) == m) return mid;
-            else if ( power(mid, n) > m) high = mid-1;
+            if ( computePower(mid, n, m) == 1) return mid;
+            else if ( computePower(mid, n, m) == 2) high = mid-1;
             else low = mid+1;
         }
         return -1;
     }
 
-    private static int power(int n, int k){
-        return (int)Math.pow(n, k);
+    // private static int power(int n, int k){
+    //     return (int)Math.pow(n, k);
+    // }
+
+    // return 1 if power == m
+    // return 0 if < m
+    // return 2 if > m
+    private static int computePower(int mid, int n, int m){
+        long ans = 1;
+        for ( int i=1; i<=n; i++){
+            ans = ans * mid;
+            if ( ans > m) return 2;
+        }
+        if ( ans == m) return 1;
+        return 0;
     }
 }
