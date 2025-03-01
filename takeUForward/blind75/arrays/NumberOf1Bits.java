10fc33f0d9cbe750bd1a9a6cfb623f1f29877290
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-03-01 14:33:54 +0530

    takeUForward/blind75/arrays/NumberOf1Bits.java

---
+++ takeUForward/blind75/arrays/NumberOf1Bits.java
@@ -0,0 +1,14 @@
+package takeUForward.blind75.arrays;
+
+public class NumberOf1Bits {
+    public int hammingWeight(int n) {
+        int mask = 1;
+        int bits = 0; // or ones
+        for ( int i=0; i<32; i++){
+            if ( (n & mask) != 0) bits++;
+            mask = mask << 1; // n = n >> 1 ( arithmetic right shift )
+            // or for signed n = n >>> 1 ( logical right shift )
+        }
+        return bits;
+    }
+}
