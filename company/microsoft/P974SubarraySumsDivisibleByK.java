41c1b9437f083f294be980930e50cb3ad673ac14
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-03-04 23:41:07 +0530

    company/microsoft/P974SubarraySumsDivisibleByK.java

--- company/microsoft/P974SubarraySumsDivisibleByK.java
+++ company/microsoft/P974SubarraySumsDivisibleByK.java
@@ -12,9 +12,10 @@ public class P974SubarraySumsDivisibleByK {
             mods[sum]++;
         }
         int ans = 0;
-        ans += (mods[0] * (mods[0]+1))/2;
+        ans += (mods[0] * (mods[0]+1))/2; // f you want to count the sum of the first x numbers: 1 + 2 + 3 + ... + x
+
         for ( int i=1; i<k; i++){
-            ans += ((mods[i]) * (mods[i]-1))/2;
+            ans += ((mods[i]) * (mods[i]-1))/2; // How many ways can we choose 2 items from x items
         }
         return ans;
     }
