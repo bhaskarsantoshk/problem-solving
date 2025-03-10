acfb82dbec84e34aa192e49d3f8f3a92c164368b
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-03-11 00:10:12 +0530

    takeUForward/blind75/linkedlists/MergeTwoSortedLists.java Iterative

--- takeUForward/blind75/linkedlists/MergeTwoSortedLists.java
+++ takeUForward/blind75/linkedlists/MergeTwoSortedLists.java
@@ -15,4 +15,29 @@ public class MergeTwoSortedLists {
             return list2;
         }
     }
+
+    public ListNode mergeTwoListsIterative(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur1 = list1, cur2 = list2;
+        ListNode cur = dummy;
+        while ( cur1 != null && cur2 != null){
+            if ( cur1.val < cur2.val) {
+                cur.next = cur1;
+                cur1 = cur1.next;
+            } else {
+                cur.next = cur2;
+                cur2 = cur2.next;
+            }
+            cur = cur.next;
+        }
+        if ( cur1 != null){
+            cur.next = cur1;
+        }
+        if ( cur2 != null){
+            cur.next = cur2;
+        }
+
+        return dummy.next;
+    }
+
 }
