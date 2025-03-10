cfb2edb57b2704712ff7d66f88c310c6d223d640
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-03-10 23:56:22 +0530

    takeUForward/blind75/linkedlists/LinkedListCycle.java

---
+++ takeUForward/blind75/linkedlists/LinkedListCycle.java
@@ -0,0 +1,17 @@
+package takeUForward.blind75.linkedlists;
+
+import company.microsoft.ListNode;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if ( head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while ( fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if ( slow == fast) return true;
+        }
+        return false;
+    }
+}
