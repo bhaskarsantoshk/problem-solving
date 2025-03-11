17f9df099f68db0b151be81ae0d8f42a2aa72d30
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-03-11 19:29:45 +0530

    takeUForward/blind75/tree/

---
+++ takeUForward/blind75/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,12 @@
+package takeUForward.blind75.tree;
+
+import company.TreeNode;
+
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if ( root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1+ Math.max( left, right);
+    }
+}
