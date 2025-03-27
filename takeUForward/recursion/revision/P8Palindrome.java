f6889a32de6d6defb32e24445b6ea4d746460f3d
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-03-27 23:32:36 +0530

    takeUForward/recursion/revision/P8Palindrome.java

---
+++ takeUForward/recursion/revision/P8Palindrome.java
@@ -0,0 +1,17 @@
+package takeUForward.recursion.revision;
+
+import java.util.Scanner;
+
+public class P8Palindrome {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String s = "MADAM";
+        System.out.println(isPalindrome(s, 0));
+    }
+
+    private static boolean isPalindrome(String s, int i) {
+        if ( i >= s.length()/2) return true;
+        if ( s.charAt(i) != s.charAt(s.length()-i-1)) return false;
+        return isPalindrome(s, i+1);
+    }
+}
