bb541a7407eeecdc00b3d2a7b3b74a7ff9fd8925
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-03-31 12:38:26 +0530

    takeUForward/recursion/revision/P16CombinationSum.java

---
+++ takeUForward/recursion/revision/P16CombinationSum.java
@@ -0,0 +1,30 @@
+package takeUForward.recursion.revision;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P16CombinationSum {
+    public static List<List< Integer >> combSum(int []candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        findCombinations(candidates, 0, target, ans, new ArrayList<>());
+        return ans;
+    }
+
+    private static void findCombinations(int[] candidates, int index, int target, List<List<Integer>> ans,
+                                         ArrayList<Integer> list) {
+        if ( index == candidates.length){
+            if ( target == 0){
+                ans.add(new ArrayList<>(list));
+            }
+            return;
+        }
+        if ( candidates[index] <= target) {
+            list.add(candidates[index]);
+            findCombinations(candidates, index, target - candidates[index], ans, list);
+            list.remove(list.size() - 1);
+        }
+        findCombinations(candidates, index+1, target, ans, list);
+    }
+}
+
+// https://www.naukri.com/code360/problems/combination-sum_981296?leftPanelTabValue=PROBLEM
