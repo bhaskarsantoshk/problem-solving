7eaa93fa122f8e1ea4a6961be76f7c072c2a1fee
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-04-10 11:57:14 +0530

    takeUForward/linkedlists/DeleteHeadOfDLL.java

---
+++ takeUForward/linkedlists/DeleteHeadOfDLL.java
@@ -0,0 +1,15 @@
+package takeUForward.linkedlists;
+
+import takeUForward.linkedlists.doubly_linked_list.ListNode;
+
+public class DeleteHeadOfDLL {
+    public ListNode deleteHead(ListNode head) {
+        if ( head == null) return null;
+        if ( head.next == null) return null;
+        ListNode prev = head;
+        head = head.next;
+        head.prev = null;
+        prev.next = null;
+        return head;
+    }
+}
