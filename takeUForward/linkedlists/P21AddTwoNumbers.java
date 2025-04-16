7533de9da88add50a48db66a7b20d9a800205e22
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-04-16 20:33:28 +0530

    takeUForward/linkedlists/P21AddTwoNumbers.java

---
+++ takeUForward/linkedlists/P21AddTwoNumbers.java
@@ -0,0 +1,49 @@
+package takeUForward.linkedlists;
+
+public class P21AddTwoNumbers {
+
+    static class LinkedListNode {
+        int data;
+        LinkedListNode next;
+
+        public LinkedListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    static LinkedListNode addTwoNumbers(LinkedListNode head1, LinkedListNode head2) {
+       int carry = 0;
+       LinkedListNode cur1 = head1;
+       LinkedListNode cur2 = head2;
+       LinkedListNode res = new LinkedListNode(0);
+       LinkedListNode dummy = res;
+       while ( cur1 != null && cur2 != null){
+           int sum = cur1.data + cur2.data + carry;
+           res.next = new LinkedListNode(sum%10);
+           res = res.next;
+           cur1 = cur1.next;
+           cur2 = cur2.next;
+           carry = sum/10;
+       }
+
+       while ( cur1 != null){
+           int sum = carry + cur1.data;
+           res.next = new LinkedListNode( sum % 10);
+           carry = sum /10;
+           res = res.next;
+           cur1 = cur1.next;
+       }
+        while ( cur2 != null){
+            int sum = carry + cur2.data;
+            res.next = new LinkedListNode( sum % 10);
+            carry = sum /10;
+            res = res.next;
+            cur2 = cur2.next;
+        }
+        if ( carry > 0){
+            res.next = new LinkedListNode(carry);
+        }
+        return dummy.next;
+    }
+
+}
