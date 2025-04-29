dbd92d690b4552cfacf8b7350c9e2ad1340652c5
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-04-29 19:39:41 +0530

    takeUForward/binaryTrees/MaximumDepthInBT.java

---
+++ takeUForward/binaryTrees/MaximumDepthInBT.java
@@ -0,0 +1,32 @@
+package takeUForward.binaryTrees;
+
+import takeUForward.trees.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepthInBT {
+    public int maxDepth(TreeNode root) {
+        if ( root == null) return 0;
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+        return 1+Math.max(lh, rh);
+    }
+
+    public int maxDepthIterative( TreeNode root) {
+        if ( root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while ( !queue.isEmpty()){
+            int size = queue.size();
+            for ( int i=0; i<size; i++){
+                TreeNode node = queue.poll();
+                if ( node.left != null) queue.offer(node.left);
+                if ( node.right != null) queue.offer(node.right);
+            }
+            level++;
+        }
+        return level;
+    }
+}
