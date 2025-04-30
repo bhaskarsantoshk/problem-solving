e530dc0cb2665175336ac4e9f6f0acd3000e4afb
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-05-01 01:38:43 +0530

    takeUForward/recursion/revision2/P9SumOfDigitsInAGivenNumber.java

--- takeUForward/recursion/revision2/P9SumOfDigitsInAGivenNumber.java
+++ takeUForward/recursion/revision2/P9SumOfDigitsInAGivenNumber.java
@@ -7,4 +7,14 @@ public class P9SumOfDigitsInAGivenNumber {
         if ( res >= 10) return addDigits(res);
         return res;
     }
+
+    public int addDigitsStriver(int num) {
+        if ( num < 10) return num;
+        return addDigitsStriver(sum(num));
+    }
+
+    private int sum(int num) {
+        if ( num < 10) return num;
+        return num%10 + sum(num/10);
+    }
 }
