70a9850cc0f299f0b0f58251ffadfe407212209e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-05-01 01:35:32 +0530

    takeUForward/recursion/revision2/P10FibonacciNumber.java

---
+++ takeUForward/recursion/revision2/P10FibonacciNumber.java
@@ -0,0 +1,8 @@
+package takeUForward.recursion.revision2;
+
+public class P10FibonacciNumber {
+    public int fib(int n) {
+        if ( n == 0 || n == 1) return n;
+        return fib(n-1)+fib(n-2);
+    }
+}
