28296382eb5421d366aa032e3b23f0ca4e2262b6
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-05-13 23:58:11 +0530

    takeUForward/dynamicProgramming/revision/P11Triangle.java

---
+++ takeUForward/dynamicProgramming/revision/P11Triangle.java
@@ -0,0 +1,36 @@
+package takeUForward.dynamicProgramming.revision;
+
+import java.util.Arrays;
+
+public class P11Triangle {
+    public int minTriangleSum(int[][] triangle) {
+        return f(0,0, triangle);
+    }
+
+    private int f(int i, int j, int[][] triangle) {
+        if ( i == triangle.length-1) return triangle[i][j];
+
+        int down = triangle[i][j]+ f(i+1, j, triangle);
+        int diag = triangle[i][j]+ f(i+1, j+1, triangle);
+        return Math.min(down, diag);
+    }
+
+    public int minTriangleSumMemoized(int[][] triangle) {
+        int m = triangle.length;
+        int [][] dp = new int[m][];
+        for ( int i=0; i<m; i++) {
+            dp[i] = new int[triangle[i].length];
+            Arrays.fill(dp[i], -1);
+        }
+        return f(0,0, triangle, dp);
+    }
+
+    private int f(int i, int j, int[][] triangle, int[][] dp) {
+        if ( i == triangle.length-1) return triangle[i][j];
+        if ( dp[i][j] != -1) return dp[i][j];
+
+        int down = triangle[i][j]+ f(i+1, j, triangle, dp);
+        int diag = triangle[i][j]+ f(i+1, j+1, triangle, dp);
+        return dp[i][j]= Math.min(down, diag);
+    }
+}
