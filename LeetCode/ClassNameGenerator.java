ab2cde41b3db73e16b03e51af388e6e99abf7d92
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-05-17 22:11:23 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="LCM of two numbers";
+        String s ="14Best time to buy and sell stock";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
