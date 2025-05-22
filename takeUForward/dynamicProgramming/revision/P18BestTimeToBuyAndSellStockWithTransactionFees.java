ee4c36108c31809e824cf7bc256dc69e0b182c0e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-05-22 16:07:05 +0530

    takeUForward/dynamicProgramming/revision/P18BestTimeToBuyAndSellStockWithTransactionFees.java

--- takeUForward/dynamicProgramming/revision/P18BestTimeToBuyAndSellStockWithTransactionFees.java
+++ takeUForward/dynamicProgramming/revision/P18BestTimeToBuyAndSellStockWithTransactionFees.java
@@ -35,4 +35,40 @@ public class P18BestTimeToBuyAndSellStockWithTransactionFees {
         }
         return dp[index][buy] = profit;
     }
+
+    public int stockBuySellTabular(int[] arr, int n, int fee) {
+        int[][] dp = new int[n+1][2];
+        dp[n][0] = dp[n][1] = 0;
+        for ( int index=n-1; index>=0; index--){
+            for ( int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if ( buy == 1){
+                    profit = Math.max(-arr[index] + dp[index+1][0]-fee, dp[index+1][1]);
+                } else {
+                    profit = Math.max( arr[index] + dp[index+1][1], dp[index+1][0]);
+                }
+                dp[index][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+
+    public int stockBuySellSpaceOptimized(int[] arr, int n, int fee) {
+        int[] ahead = new int[2];
+        int[] cur = new int[2];
+        ahead[0] = ahead[1] = 0;
+        for ( int index=n-1; index>=0; index--){
+            for ( int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if ( buy == 1){
+                    profit = Math.max(-arr[index] + ahead[0]-fee, ahead[1]);
+                } else {
+                    profit = Math.max( arr[index] + ahead[1], ahead[0]);
+                }
+                cur[buy] = profit;
+            }
+            ahead = cur;
+        }
+        return cur[1];
+    }
 }
