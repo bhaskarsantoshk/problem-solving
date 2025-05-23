2a603169db6ea0b7755d40485bdc9c1d06de20d7
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-05-23 16:29:45 +0530

    takeUForward/dynamicProgramming/revision/P19SubsetSumEqualsToTarget.java

--- takeUForward/dynamicProgramming/revision/P19SubsetSumEqualsToTarget.java
+++ takeUForward/dynamicProgramming/revision/P19SubsetSumEqualsToTarget.java
@@ -4,35 +4,65 @@ import java.util.Arrays;
 
 public class P19SubsetSumEqualsToTarget {
     public boolean isSubsetSum(int[] arr, int target) {
-        return isSubsetSum( 0, 0, arr, arr.length, target);
+        return f(arr.length-1, target, arr);
     }
 
-    private boolean isSubsetSum(int index, int sum, int[] arr, int length, int target) {
-        if ( index == length) {
-            if ( sum == target) return true;
-        }
-        boolean take =  isSubsetSum(index+1, sum+ arr[index], arr, length, target);
+    private boolean f(int index, int target, int[] arr) {
+        if ( target == 0) return true;
+        if ( index == 0) return arr[index] == target;
+        boolean take = f(index-1, target-arr[index], arr);
         if ( take) return true;
-        boolean notTake = isSubsetSum(index+1, sum, arr, length, target);
-        if ( notTake) return true;
-        return false;
+        return f(index+1, target, arr);
     }
 
     public boolean isSubsetSumMemoized(int[] arr, int target) {
         Boolean[][] dp = new Boolean[arr.length][target+1];
-        return isSubsetSum( 0, 0, arr, arr.length, target, dp);
+        return f(arr.length-1, target, arr);
     }
 
-    private boolean isSubsetSum(int index, int sum, int[] arr, int length, int target, Boolean[][] dp) {
-        if ( sum > target ) return false;
-        if ( index == length) {
-            if ( sum == target) return true;
+    private boolean f(int index, int target, int[] arr, Boolean[][] dp) {
+        if ( target == 0) return true;
+        if ( target < 0) return false;
+        if ( index == 0) return arr[index] == target;
+        if ( dp[index][target] != null) return dp[index][target];
+        boolean take = f(index-1, target-arr[index], arr, dp);
+        if ( take) return dp[index][target] = true;
+        boolean notTake = f(index-1, target, arr, dp);
+        return notTake;
+    }
+
+    public boolean isSubsetSumTabulation(int[] arr, int target) {
+        int n = arr.length;
+        boolean[][] dp = new boolean[arr.length][target+1];
+        for ( int i=0; i <n; i++) dp[i][0] = true;
+        if ( arr[0] <= target) dp[0][arr[0]] = true;
+        for ( int index=1; index<n; index++){
+            for ( int sum=1; sum<=target; sum++){
+                boolean take = false;
+                if(sum-arr[index] >=0) take = dp[index-1][sum-arr[index]];
+                boolean notTake = dp[index-1][sum];
+                dp[index][sum] = take || notTake;
+            }
         }
-        if ( dp[index][sum] != null) return dp[index][sum];
-        boolean take =  isSubsetSum(index+1, sum+ arr[index], arr, length, target, dp);
-        if ( take) return dp[index][sum] = true;
-        boolean notTake = isSubsetSum(index+1, sum, arr, length, target, dp);
-        if ( notTake) return dp[index][sum] = true;
-        return dp[index][sum] = false;
+        return dp[n-1][target];
+    }
+
+    public boolean isSubsetSumSpaceOptimization(int[] arr, int target) {
+        int n = arr.length;
+        boolean[] prev = new boolean[target+1];
+        prev[0] = true;
+        if ( arr[0] <= target) prev[arr[0]] = true;
+        for ( int index=1; index<n; index++){
+            boolean[] cur = new boolean[target+1];
+            cur[0] = true;
+            for ( int sum=1; sum<=target; sum++){
+                boolean take = false;
+                if(sum-arr[index] >=0) take = prev[sum-arr[index]];
+                boolean notTake = prev[sum];
+                cur[sum] = take || notTake;
+            }
+            prev = cur;
+        }
+        return prev[target];
     }
 }
