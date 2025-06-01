01a4c7193deac14ee9e9f835aa5a8ba1c246de60
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-01 23:15:16 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="25Minimum coins\n";
+        String s ="31Longest Increasing Subsequence\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
