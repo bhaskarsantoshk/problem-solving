d9a4d197d796480220ba79dff0195333b912f5ba
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-02 23:58:27 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="31Longest Increasing Subsequence\n";
+        String s ="Largest Odd Number in a String\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
