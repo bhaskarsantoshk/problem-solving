141bf1cee75a1b185ab0b7c694b7ead917f86522
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-05 22:07:33 +0530

    takeUForward/dynamicProgramming/revision/P30UnboundedKnapsack.java

---
+++ takeUForward/dynamicProgramming/revision/P30UnboundedKnapsack.java
@@ -0,0 +1,42 @@
+package takeUForward.dynamicProgramming.revision;
+
+import java.util.Arrays;
+
+public class P30UnboundedKnapsack {
+    public int unboundedKnapsack(int[] wt, int[] val, int n, int W) {
+        return f(0, wt, val, W);
+    }
+
+    private int f(int index, int[] wt, int[] val, int w) {
+        if ( index == wt.length){
+            return 0;
+        }
+        int notTake = f(index+1, wt, val, w);
+        int take = 0;
+        if ( w-wt[index] >=0){
+            take = f(index, wt, val, w-wt[index])+val[index];
+        }
+        return Math.max(take, notTake);
+    }
+
+    public int unboundedKnapsackMemoized(int[] wt, int[] val, int n, int W) {
+        int [][] dp = new int[n][W+1];
+        for ( int i=0; i<n; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return f(0, wt, val, W, dp);
+    }
+
+    private int f(int index, int[] wt, int[] val, int w, int[][] dp) {
+        if ( index == wt.length){
+            return 0;
+        }
+        if ( dp[index][w] != -1) return dp[index][w];
+        int notTake = f(index+1, wt, val, w);
+        int take = 0;
+        if ( w-wt[index] >=0){
+            take = f(index, wt, val, w-wt[index])+val[index];
+        }
+        return dp[index][w] = Math.max(take, notTake);
+    }
+}
