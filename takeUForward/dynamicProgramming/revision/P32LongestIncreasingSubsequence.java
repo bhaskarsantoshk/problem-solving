1b1390194f874148f1d6b5d3cea13a833992f0d1
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-08 23:50:21 +0530

    takeUForward/dynamicProgramming/revision/P32LongestIncreasingSubsequence.java

--- takeUForward/dynamicProgramming/revision/P32LongestIncreasingSubsequence.java
+++ takeUForward/dynamicProgramming/revision/P32LongestIncreasingSubsequence.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class P32LongestIncreasingSubsequence {
     public int LIS(int[] nums) {
-        return f(nums.length-1, nums.length, nums);
+        return f(0, -1, nums);
     }
 
     private int f(int index, int prevIndex, int[] nums) {
-        if ( index == -1) return 0;
-        int notTake = f( index-1, prevIndex, nums);
+        if ( index == nums.length) return 0;
+        int notTake = f( index+1, prevIndex, nums);
         int take = 0;
-        if ( prevIndex == nums.length || nums[index] > nums[prevIndex]) {
-            take = f(index-1, index, nums);
+        if ( prevIndex == -1|| nums[index] > nums[prevIndex]) {
+            take = f(index+1, index, nums);
         }
         return Math.max(take, notTake);
     }
@@ -21,20 +21,25 @@ public class P32LongestIncreasingSubsequence {
         int n = nums.length;
         int dp[][] = new int[n][n+1];
         for ( int i=0; i<n; i++) Arrays.fill(dp[i], -1);
-        return f(n-1, n, nums, dp);
+        return f(0, -1, nums , dp);
     }
 
     private int f(int index, int prevIndex, int[] nums, int[][] dp) {
-        if ( index == -1) {
+        if ( index == nums.length) {
             return 0;
         }
-        if ( dp[index][prevIndex] != -1) return dp[index][prevIndex];
-        int notTake = f( index-1, prevIndex, nums, dp);
+        if ( dp[index][prevIndex+1] != -1) return dp[index][prevIndex+1];
+        int notTake = f( index+1, prevIndex, nums, dp);
         int take = 0;
-        if ( prevIndex == nums.length || nums[index] < nums[prevIndex]) {
-            take = f(index-1, index, nums, dp);
+        if ( prevIndex == -1 || nums[index] > nums[prevIndex]) {
+            take = 1+f(index+1, index, nums, dp);
         }
-        return dp[index][prevIndex] = Math.max(take, notTake);
+        return dp[index][prevIndex+1] = Math.max(take, notTake);
     }
+
+    public int LISTabular(int[] nums) {
+        return 0;
+    }
+
 
 }
