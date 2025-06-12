cbf12b4e089105269a5223a8d5d1448730d4fc11
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-12 16:42:50 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="31Rod cutting problem\n";
+        String s ="Print Longest Increasing Subsequence\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
