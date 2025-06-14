a8afdfe1d37e5d9ebe41080907fdb26d58e5544f
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-14 23:06:40 +0530

    takeUForward/dynamicProgramming/revision/P38LongestCommonSubsequence.java

--- takeUForward/dynamicProgramming/revision/P38LongestCommonSubsequence.java
+++ takeUForward/dynamicProgramming/revision/P38LongestCommonSubsequence.java
@@ -27,5 +27,18 @@ public class P38LongestCommonSubsequence {
         return dp[index1][index2] = Math.max(f(index1-1, index2, str1, str2, dp), f(index1, index2-1, str1, str2, dp));
     }
 
+    public int lcsTabular( String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        int [][] dp = new int[m+1][n+1];
+        for ( int i=1; i<=m; i++){
+            for ( int j=1; j<=n; j++){
+                if ( str1.charAt(i-1) == str2.charAt(j-1)) dp[i][j] = 1+ dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
+    }
+
 
 }
