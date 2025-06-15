badd68e4fa34ffe1abc75d1ac1966dea7ebc86f3
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-15 11:41:15 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="Print Longest Increasing Subsequence\n";
+        String s ="41Minimum insertions to make string palindrome\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
