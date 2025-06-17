a564fb632b58d74b215a9b1c4959b994ff4f18f7
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-17 11:42:48 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="46Wildcard matching\n";
+        String s ="47Matrix chain multiplication\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
