580bc3f4e6dcbd35b7c0cb26dde7513e14fd9310
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-18 22:23:29 +0530

    takeUForward/twoPointers/P1SlidingWindowSum.java

---
+++ takeUForward/twoPointers/P1SlidingWindowSum.java
@@ -0,0 +1,23 @@
+package takeUForward.twoPointers;
+
+public class P1SlidingWindowSum {
+    public static int maxSumWindowK(int[] arr, int k) {
+        int n = arr.length;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int l=0, r=0;
+        while ( r < k){
+            sum += arr[r];
+            r++;
+        }
+        max = Math.max(max, sum);
+        while ( r < n){
+            sum -= arr[l];
+            l++;
+            sum += arr[r];
+            r++;
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
