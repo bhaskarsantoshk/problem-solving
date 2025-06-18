9d29e22cc17814e1fd86d7247e7bd732861e6d3f
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-18 22:56:19 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="50Palindrome partitioning II\n";
+        String s ="3Number of Subarrays with Sum K\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
