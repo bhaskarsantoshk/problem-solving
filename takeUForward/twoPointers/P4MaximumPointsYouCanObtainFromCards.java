2a5560afce4785ca1deca3b43aa3efbee915b42d
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-19 10:45:16 +0530

    takeUForward/twoPointers/P4MaximumPointsYouCanObtainFromCards.java

---
+++ takeUForward/twoPointers/P4MaximumPointsYouCanObtainFromCards.java
@@ -0,0 +1,19 @@
+package takeUForward.twoPointers;
+
+public class P4MaximumPointsYouCanObtainFromCards {
+    public int maxScore(int[] cardScore, int k) {
+       int leftSum =0, rightSum=0, maxSum=0;
+       for ( int i=0; i<=k-1; i++){
+           leftSum += cardScore[i];
+       }
+       maxSum = leftSum;
+       int rightIndex = cardScore.length-1;
+       for ( int i=k-1; i>=0; i--){
+           leftSum -= cardScore[i];
+           rightSum += cardScore[rightIndex];
+           maxSum = Math.max(maxSum, leftSum+rightSum);
+           rightIndex--;
+       }
+       return maxSum;
+    }
+}
