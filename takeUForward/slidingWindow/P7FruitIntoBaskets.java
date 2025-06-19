8350fe723d58d4ae6217c13713744f81d083ca86
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-19 19:16:43 +0530

    takeUForward/slidingWindow/P7FruitIntoBaskets.java

--- takeUForward/slidingWindow/P7FruitIntoBaskets.java
+++ takeUForward/slidingWindow/P7FruitIntoBaskets.java
@@ -1,11 +1,14 @@
 package takeUForward.slidingWindow;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class P7FruitIntoBaskets {
     public int totalFruits(int[] fruits) {
-        return longestSubArrayWithAtmostKDistinctNumbers(fruits, 2);
+        // return longestSubArrayWithAtmostKDistinctNumbers(fruits, 2);
+        return longestSubArrayWithAtmostKDistinctNumbersBetter(fruits, 2);
     }
 
     private int longestSubArrayWithAtmostKDistinctNumbers(int[] fruits, int k) {
@@ -18,6 +21,24 @@ public class P7FruitIntoBaskets {
                 if ( set.size() <= k) maxLen = Math.max(maxLen, j-i+1);
                 else break;
             }
+        }
+        return maxLen;
+    }
+
+    private int longestSubArrayWithAtmostKDistinctNumbersBetter(int[] fruits, int k) {
+        int n = fruits.length;
+        int maxLen = 0;
+        int left =0, right =0;
+        Map<Integer, Integer> hash = new HashMap<>();
+        while ( right < n){
+            hash.put(fruits[right], hash.getOrDefault(fruits[right],0)+1);
+            while ( hash.size() > k){
+                hash.put(fruits[left], hash.getOrDefault(fruits[left],0)-1);
+                if ( hash.get(fruits[left]) == 0) hash.remove(fruits[left]);
+                left++;
+            }
+            if ( hash.size() <=k ) maxLen = Math.max(maxLen, right-left+1);
+            right++;
         }
         return maxLen;
     }
