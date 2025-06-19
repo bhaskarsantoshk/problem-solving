0aef5661f5ac64fec49d334040595338e8c49cd8
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-19 19:39:20 +0530

    takeUForward/slidingWindow/P7FruitIntoBaskets.java

--- takeUForward/slidingWindow/P7FruitIntoBaskets.java
+++ takeUForward/slidingWindow/P7FruitIntoBaskets.java
@@ -42,4 +42,22 @@ public class P7FruitIntoBaskets {
         }
         return maxLen;
     }
+
+    private int longestSubArrayWithAtmostKDistinctNumbersOptimal(int[] fruits, int k) {
+        int n = fruits.length;
+        int maxLen = 0;
+        int left =0, right =0;
+        Map<Integer, Integer> hash = new HashMap<>();
+        while ( right < n){
+            hash.put(fruits[right], hash.getOrDefault(fruits[right],0)+1);
+            if ( hash.size() > k){
+                hash.put(fruits[left], hash.getOrDefault(fruits[left],0)-1);
+                if ( hash.get(fruits[left]) == 0) hash.remove(fruits[left]);
+                left++;
+            }
+            if ( hash.size() <=k ) maxLen = Math.max(maxLen, right-left+1);
+            right++;
+        }
+        return maxLen;
+    }
 }
