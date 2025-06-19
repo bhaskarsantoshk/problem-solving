8b0de24b2f134d46a4a656f04928f0a6d7ddb716
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-19 15:58:12 +0530

    takeUForward/twoPointers/P6MaxConsecutiveOnesIII.java

--- takeUForward/twoPointers/P6MaxConsecutiveOnesIII.java
+++ takeUForward/twoPointers/P6MaxConsecutiveOnesIII.java
@@ -2,7 +2,8 @@ package takeUForward.twoPointers;
 
 public class P6MaxConsecutiveOnesIII {
     public int longestOnes(int[] nums, int k) {
-        return longestSubStringWithAtMostKZerosBruteForce(nums, k);
+        // return longestSubStringWithAtMostKZerosBruteForce(nums, k);
+        return longestSubStringWithAtMostKZerosSlidingWindow(nums, k);
     }
 
     private int longestSubStringWithAtMostKZerosBruteForce(int[] nums, int k) {
@@ -15,6 +16,22 @@ public class P6MaxConsecutiveOnesIII {
                 if ( zeros <= k) maxLen = Math.max(maxLen, j-i+1);
                 else break;
             }
+        }
+        return maxLen;
+    }
+
+    private int longestSubStringWithAtMostKZerosSlidingWindow(int[] nums, int k) {
+        int maxLen = 0;
+        int n = nums.length;
+        int left =0, right =0, zeros=0;
+        while ( right < n){
+            if ( nums[right] == 0) zeros++;
+            while ( zeros>k){
+                if ( nums[left] == 0) zeros--;
+                left++;
+            }
+            maxLen = Math.max(maxLen, right-left+1);
+            right++;
         }
         return maxLen;
     }
