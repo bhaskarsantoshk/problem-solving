877894c4062cc3032e7fe38a3e72eb2be1c33353
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-20 19:11:32 +0530

    takeUForward/slidingWindow/P11NumberOfSubstringsContainingAllThreeCharacters.java

---
+++ takeUForward/slidingWindow/P11NumberOfSubstringsContainingAllThreeCharacters.java
@@ -0,0 +1,37 @@
+package takeUForward.slidingWindow;
+
+public class P11NumberOfSubstringsContainingAllThreeCharacters {
+    public int numberOfSubstrings(String s) {
+       int result = 0;
+       int n = s.length();
+       for ( int i=0; i<n; i++){
+           int []hash = new int[3];
+           for ( int j=i; j<n; j++){
+               char c = s.charAt(j);
+               hash[c-'a']=1;
+               if ( hash[0]+hash[1]+hash[2] == 3) {
+                   result ++;
+               }
+           }
+       }
+       return result;
+    }
+
+    public int numberOfSubstringsBetter(String s) {
+        int result = 0;
+        int n = s.length();
+        for ( int i=0; i<n; i++){
+            int []hash = new int[3];
+            for ( int j=i; j<n; j++){
+                char c = s.charAt(j);
+                hash[c-'a']=1;
+                if ( hash[0]+hash[1]+hash[2] == 3) {
+                    result += (n-j);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
+
