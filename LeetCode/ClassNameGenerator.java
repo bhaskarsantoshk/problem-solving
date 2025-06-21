f1139f1adfc10c1cd6066cdabb3a55311468fe67
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-21 07:41:04 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="13Count Subarray sum Equals K \n";
+        String s ="14Count number of Nice subarrays\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
