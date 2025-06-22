53fcc345684425862518b14b1ecc7e99a0f8b158
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-22 07:31:06 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="14Count number of Nice subarrays\n";
+        String s ="1Reverse every word in a string\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
