e48af8aa36b6fca882a87c0767013afb11a3bf83
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-23 20:57:45 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="3Count and say";
+        String s ="4Rabin Karp Algorithm";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
