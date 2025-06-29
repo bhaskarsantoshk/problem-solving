b0dec5e1bf1fae0649e345a4674f1981fc576127
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-06-29 10:30:00 +0530

    takeUForward/dynamicProgramming/revision3/P11CherryPickupII.java

--- takeUForward/graph/revision2/P13TopologicalSortOrKahnSAlgorithm.java
+++ takeUForward/graph/revision2/P13TopologicalSortOrKahnSAlgorithm.java
@@ -1,6 +1,8 @@
 package takeUForward.graph.revision2;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 public class P13TopologicalSortOrKahnSAlgorithm {
@@ -24,5 +26,31 @@ public class P13TopologicalSortOrKahnSAlgorithm {
             if ( !vis[adjNode]) dfs(adjNode, adj, vis, stack);
         }
         stack.push(node);
+    }
+
+    public int[] topoSortKahnAlgorithm(int V, List<List<Integer>> adj) {
+        Queue<Integer> queue = new LinkedList<>();
+        int[] ans = new int[V];
+        int index = 0;
+        int inDegree[] = new int[V];
+        for ( int i=0; i<V; i++){
+            for ( int adjNode: adj.get(i)){
+                inDegree[adjNode]++;
+            }
+        }
+        for ( int i=0; i<V; i++){
+            if ( inDegree[i] == 0) queue.offer(i);
+        }
+        while ( !queue.isEmpty()){
+           int node = queue.poll();
+           ans[index++] = node;
+           for ( int adjNode: adj.get(node)){
+               inDegree[adjNode]--;
+               if ( inDegree[adjNode] == 0){
+                   queue.offer(adjNode);
+               }
+           }
+        }
+        return ans;
     }
 }
