9cf2e7c7a4fa53ef1d466038be9b28eb15e6a132
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-07-05 15:42:03 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="6KMP Algorithm or LPS array\n";
+        String s ="Longest happy prefix\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
