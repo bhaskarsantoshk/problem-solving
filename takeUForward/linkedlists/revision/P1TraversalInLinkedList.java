269e8cb5139e681076693027d149c779ca52aaf7
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-07-05 21:48:36 +0530

    takeUForward/linkedlists/revision/P1TraversalInLinkedList.java

--- takeUForward/linkedlists/revision/P1TraversalInLinkedList.java
+++ takeUForward/linkedlists/revision/P1TraversalInLinkedList.java
@@ -1,2 +1,16 @@
-package takeUForward.linkedlists.revision;public class P1TraversalInLinkedList {
+package takeUForward.linkedlists.revision;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P1TraversalInLinkedList {
+    public List<Integer> LLTraversal(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode cur = head;
+        while ( cur != null ){
+            list.add(cur.val);
+            cur = cur.next;
+        }
+        return list;
+    }
 }
