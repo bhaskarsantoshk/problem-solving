d45ef1d4ab2ad146dad50672a25beda9dd19ee41
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-07-06 21:06:16 +0530

    takeUForward/linkedlists/revision/P5DeleteTheElementWithValueX.java

---
+++ takeUForward/linkedlists/revision/P5DeleteTheElementWithValueX.java
@@ -0,0 +1,23 @@
+package takeUForward.linkedlists.revision;
+
+public class P5DeleteTheElementWithValueX {
+    public ListNode deleteNodeWithValueX(ListNode head, int X) {
+        if ( head == null) return null;
+        if ( head.val == X) {
+            ListNode result = head.next;
+            head = null;
+            return result;
+            // simply return head.next works too
+        }
+        ListNode cur = head, prev = null;
+        while ( cur != null){
+            if ( cur.val == X){
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
+        return head;
+    }
+}
