350f60144f0b34df805708148ab576631d92d907
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-07-06 21:06:17 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="Longest happy prefix\n";
+        String s ="Insertion before the value X in LL\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
