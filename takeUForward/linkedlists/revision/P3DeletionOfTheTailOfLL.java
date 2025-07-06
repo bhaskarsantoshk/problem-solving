a84a4e03d1157b5c3abaaa132f8adffd8b0efa48
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-07-06 21:06:16 +0530

    takeUForward/linkedlists/revision/P3DeletionOfTheTailOfLL.java

---
+++ takeUForward/linkedlists/revision/P3DeletionOfTheTailOfLL.java
@@ -0,0 +1,13 @@
+package takeUForward.linkedlists.revision;
+
+public class P3DeletionOfTheTailOfLL {
+    public ListNode deleteTail(ListNode head) {
+        if ( head == null || head.next == null) return null;
+        ListNode cur = head;
+        while ( cur.next.next != null){
+            cur = cur.next;
+        }
+        cur.next = null;
+        return head;
+    }
+}
