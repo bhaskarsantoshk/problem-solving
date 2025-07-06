86d9389c6d19b58bab157e4400aa78c5bca5ca8a
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-07-06 21:06:16 +0530

    takeUForward/linkedlists/revision/P8InsertionAtTheKthPositionOfLL.java

---
+++ takeUForward/linkedlists/revision/P8InsertionAtTheKthPositionOfLL.java
@@ -0,0 +1,22 @@
+package takeUForward.linkedlists.revision;
+
+public class P8InsertionAtTheKthPositionOfLL {
+    public ListNode insertAtKthPosition(ListNode head, int X, int K) {
+        if ( head == null){
+            if ( K == 1) return new ListNode(X);
+            return null;
+        }
+        if ( K == 1) return new ListNode(X, head);
+        ListNode cur = head;
+        int count = 0;
+        while ( cur.next != null){
+            count++;
+            if ( count == K-1) {
+                cur.next = new ListNode(X, cur.next);
+                break;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
