d3bb87399ade533aa37a83a7def43db37e62eb65
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-07-08 23:58:05 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="11Delete Tail of DLL\n";
+        String s ="14Insert node before head in DLL\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
