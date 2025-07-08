f1920461cc6de8a590ed41fa1f4fec526fb49e1d
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-07-08 22:35:23 +0530

    takeUForward/linkedlists/revision/doubly_linked_list/ListNode.java

---
+++ takeUForward/linkedlists/revision/doubly_linked_list/ListNode.java
@@ -0,0 +1,25 @@
+package takeUForward.linkedlists.revision.doubly_linked_list;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode prev;
+
+    ListNode() {
+        val = 0;
+        next = null;
+        prev = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+        prev = null;
+    }
+
+    ListNode(int data1, ListNode next1, ListNode prev1) {
+        val = data1;
+        next = next1;
+        prev = prev1;
+    }
+}
