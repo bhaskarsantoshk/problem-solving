f3779a80f24652422b31a392de1b13b10e148e4c
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-07-17 10:18:19 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="7Rotten Oranges";
+        String s ="9Surrounded Regions";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
