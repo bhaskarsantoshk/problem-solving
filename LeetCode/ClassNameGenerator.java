9db18ff18f194726c78e061bdda45e4615264af0
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-07-25 19:03:05 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="79. Word Search\n";
+        String s ="212. Word Search II\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
