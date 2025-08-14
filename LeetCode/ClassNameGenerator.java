83055ac7303f96846a247b5d3020ad4dcc88d11d
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-08-14 23:27:56 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="11Detect a cycle in an undirected graph\n";
+        String s ="12Bipartite graph\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
