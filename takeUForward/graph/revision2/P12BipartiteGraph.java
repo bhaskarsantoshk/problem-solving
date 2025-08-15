f546e890ed377366b8d6b7346266829a7f63c035
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-08-15 07:47:15 +0530

    takeUForward/graph/revision2/P12BipartiteGraph.java

--- takeUForward/graph/revision2/P12BipartiteGraph.java
+++ takeUForward/graph/revision2/P12BipartiteGraph.java
@@ -35,4 +35,26 @@ public class P12BipartiteGraph {
         }
         return true;
     }
+
+    public boolean isBipartiteDFS(int V, List<List<Integer>> adj) {
+        int[] colors = new int[V];
+        Arrays.fill(colors, -1);
+        for ( int i=0; i<V; i++){
+            if ( colors[i] == -1){
+                if (!checkIfBiPartiteDFS(adj, i,0, colors)) return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkIfBiPartiteDFS(List<List<Integer>> adj, int node, int color, int[] colors) {
+        colors[node] = color;
+        for ( int adjNode: adj.get(node)){
+            if ( colors[adjNode] == -1) {
+                if (!checkIfBiPartiteDFS(adj, adjNode, 1 - color, colors)) return false;
+            }
+            else if ( colors[node] == colors[adjNode]) return false;
+        }
+        return true;
+    }
 }
