079f85481026db79681a29837a1c8479732646c5
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-08-17 01:15:24 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="13Topological sort or Kahn's algorithm\n";
+        String s ="15Find eventual safe states\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
