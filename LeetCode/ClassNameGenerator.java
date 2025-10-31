f9200efb7319b3b69cc3bdd704ef2a0ea6110c72
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-10-31 22:58:06 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="P2Sort an array of 0's 1's and 2's\n";
+        String s ="3Kadane's Algorithm";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
