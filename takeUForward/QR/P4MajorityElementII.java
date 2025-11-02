d6dce1568c365245eba38c7a317f993b1dc083e7
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-11-02 13:09:50 +0530

    takeUForward/QR/P4MajorityElementII.java

--- takeUForward/QR/P4MajorityElementII.java
+++ takeUForward/QR/P4MajorityElementII.java
@@ -41,6 +41,33 @@ public class P4MajorityElementII {
 
     public List<Integer> majorityElementTwoOptimal(int[] nums) {
         List<Integer> res = new ArrayList<>();
+        int count1 = 0, count2=0;
+        int element1 = Integer.MIN_VALUE, element2 = Integer.MIN_VALUE;
+        int n = nums.length;
+        for ( int i=0; i<n; i++){
+            if ( count1 == 0 && nums[i] != element2){
+                count1 = 1;
+                element1 = nums[i];
+            } else if ( count2 == 0 && nums[i] != element1){
+                count2=1;
+                element2 = nums[i];
+            } else if ( nums[i] == element1) count1++;
+            else if ( nums[i] == element2) count2++;
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        int cnt1=0, cnt2=0;
+        for ( int i=0; i<n; i++){
+            if ( nums[i] == element1) cnt1++;
+            if ( nums[i] == element2) cnt2++;
+        }
+
+        if ( cnt1 > n/3) res.add(element1);
+        if ( cnt2 > n/3) res.add(element2);
+
         return res;
     }
 }
