650d89b30964ea55c1c54dda41773a8747742015
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-11-06 14:22:44 +0530

    takeUForward/QR/P8SearchInRotatedSortedArrayII.java

--- takeUForward/QR/P8SearchInRotatedSortedArrayII.java
+++ takeUForward/QR/P8SearchInRotatedSortedArrayII.java
@@ -2,7 +2,22 @@ package takeUForward.QR;
 
 public class P8SearchInRotatedSortedArrayII {
     public boolean searchInARotatedSortedArrayII(int[] nums, int k) {
-
+        int low = 0, high = nums.length-1;
+        while ( low <= high){
+            int mid = (low + high)/2;
+            if ( nums[mid] == k) return true;
+            if ( nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++; high--;
+                continue;
+            } else if ( nums[low] <= nums[mid]){
+                if ( nums[low] <= k && k <= nums[mid]) high = mid-1;
+                else low = mid+1;
+            } else {
+                if ( nums[mid] <= k && k<= nums[high]) low= mid+1;
+                else high = mid-1;
+            }
+        }
+        return false;
     }
 
     public int searchInARotatedSortedArrayI(int[] nums, int k) {
