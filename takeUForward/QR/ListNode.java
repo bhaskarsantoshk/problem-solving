f344ad947d5d019c19962452f452cfb2d3ff0b26
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-11-15 11:36:12 +0530

    takeUForward/QR/ListNode.java

---
+++ takeUForward/QR/ListNode.java
@@ -0,0 +1,16 @@
+package takeUForward.QR;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
