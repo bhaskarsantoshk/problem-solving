7f5038cf6e4eadf5f65ef3bd85be08d77586d35f
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-11-18 23:59:16 +0530

    takeUForward/QR/P36LCAInBT.java

---
+++ takeUForward/QR/P36LCAInBT.java
@@ -0,0 +1,12 @@
+package takeUForward.QR;
+
+public class P36LCAInBT {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if ( root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if ( left == null) return right;
+        else if ( right == null) return left;
+        else return root;
+    }
+}
