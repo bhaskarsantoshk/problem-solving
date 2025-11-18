45cce4dc70cfc86754a8ee42ba32149681a00876
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-11-18 23:59:16 +0530

    takeUForward/QR/TreeNode.java

---
+++ takeUForward/QR/TreeNode.java
@@ -0,0 +1,12 @@
+package takeUForward.QR;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
