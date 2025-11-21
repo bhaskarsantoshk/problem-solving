5df4dc5af0df0222d4f5d224cc9005ed9cd47c61
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-11-21 23:56:51 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="38Construct a BT from Preorder and Inorder\n";
+        String s ="39Minimum time taken to burn ...\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
