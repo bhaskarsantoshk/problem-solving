b2310d9f2d0486bf6069f7e0899fc4f926c1cb97
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-12-02 23:52:19 +0530

    takeUForward/recursion/revision4/P3PrintNtoOne.java

---
+++ takeUForward/recursion/revision4/P3PrintNtoOne.java
@@ -0,0 +1,17 @@
+package takeUForward.recursion.revision4;
+
+import java.util.Scanner;
+
+public class P3PrintNtoOne {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        f(n, n);
+    }
+
+    private static void f(int i, int n) {
+        if ( i < 1) return;
+        System.out.println(i);
+        f(i-1, n);
+    }
+}
