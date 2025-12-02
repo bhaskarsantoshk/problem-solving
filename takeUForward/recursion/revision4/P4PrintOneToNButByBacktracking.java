d9519727597f6fe25d1c61e0d7dc532839a2e50a
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-12-02 23:56:22 +0530

    takeUForward/recursion/revision4/P4PrintOneToNButByBacktracking.java

---
+++ takeUForward/recursion/revision4/P4PrintOneToNButByBacktracking.java
@@ -0,0 +1,17 @@
+package takeUForward.recursion.revision4;
+
+import java.util.Scanner;
+
+public class P4PrintOneToNButByBacktracking {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        f(n);
+    }
+
+    private static void f(int n){
+        if ( n == 0) return;
+        f(n-1);
+        System.out.println(n);
+    }
+}
