ed3613422fc0617a24df2948ca5435151c627668
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-12-04 20:43:50 +0530

    takeUForward/recursion/revision4/P9Fibonacci.java

---
+++ takeUForward/recursion/revision4/P9Fibonacci.java
@@ -0,0 +1,17 @@
+package takeUForward.recursion.revision4;
+
+import java.util.Scanner;
+
+public class P9Fibonacci {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(fib(n));
+    }
+
+    private static int fib(int n) {
+        if ( n <= 1) return n;
+        return fib(n-1) + fib(n-2);
+    }
+    // T: O(2^N) S:O(N)
+}
