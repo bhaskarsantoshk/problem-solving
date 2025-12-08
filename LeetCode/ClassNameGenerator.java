29a2d8bfe605d5f7c0bf29e31f9685f16d0ef1d6
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-12-08 23:11:55 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="Add Two Polynomials Represented as Linked Lists\n";
+        String s ="All Paths from Source Lead to Destination\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
