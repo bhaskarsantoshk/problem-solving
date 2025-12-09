b641044a6719b14768012f399202094589569d24
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-12-09 23:48:22 +0530

    takeUForward/google/PFindRowWithMaximum1S.java

--- takeUForward/google/PFindRowWithMaximum1S.java
+++ takeUForward/google/PFindRowWithMaximum1S.java
@@ -2,29 +2,52 @@ package takeUForward.google;
 
 public class PFindRowWithMaximum1S {
     public int rowWithMax1sBruteForce(int[][] mat) {
-        int maxRow = 0;
+        int maxRow = -1;
         int count = 0;
-        int maxCount = -1;
+        int maxCount = 0;
         for ( int i=0; i<mat.length; i++){
             count = 0;
             for ( int j=0; j<mat[0].length; j++){
                 count+= mat[i][j];
             }
-            if ( maxCount > count){
+            if ( maxCount < count){
                 maxRow = i;
                 maxCount = count;
             }
         }
-        return maxRow == 0 ? -1: maxRow;
+        return maxRow;
     }
 
     public int rowWithMax1sBinarySearch(int[][] mat) {
-        int maxRow = 0;
-        int count = 0;
-        int maxCount = -1;
-        for ( int i=0; i<mat.length; i++){
-
+        int maxRow = -1;
+        int maxCount = 0;
+        int m = mat.length;
+        int n = mat[0].length;
+        for ( int i=0; i<m; i++){
+            int index = lowerBound(mat[i], 1);
+            if ( index < n){
+                int count = n - index;
+                if (count > maxCount) {
+                    maxCount = count;
+                    maxRow = i;
+                }
+            }
         }
-        return maxRow == 0 ? -1: maxRow;
+        return maxRow;
+    }
+
+    private int lowerBound(int[] arr, int key) {
+        int start = 0, end = arr.length-1;
+        int result = arr.length;
+        while ( start <= end ){
+            int mid = start + ( end-start)/2;
+            if ( arr[mid] >= key){
+                result = mid;
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+        return result;
     }
 }
