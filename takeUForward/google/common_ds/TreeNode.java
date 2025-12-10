9d8b1190ebe489a247d591b1aa301e60dac2bde1
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-12-10 23:31:15 +0530

    takeUForward/google/common_ds/TreeNode.java

---
+++ takeUForward/google/common_ds/TreeNode.java
@@ -0,0 +1,10 @@
+package takeUForward.google.common_ds;
+
+public class TreeNode {
+    public int data;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int data){
+        this.data = data;
+    }
+}
