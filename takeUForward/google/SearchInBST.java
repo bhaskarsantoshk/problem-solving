344b06aba02931627754afecebfdb58fe6cfbbe1
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-12-11 23:37:27 +0530

    takeUForward/google/SearchInBST.java

---
+++ takeUForward/google/SearchInBST.java
@@ -0,0 +1,11 @@
+package takeUForward.google;
+import takeUForward.google.common_ds.TreeNode
+
+public class SearchInBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if ( root == null) return null;
+        if ( root.data == val) return root;
+        if ( root.data < val ) return searchBST(root.right, val);
+        return searchBST(root.left, val);
+    }
+}
