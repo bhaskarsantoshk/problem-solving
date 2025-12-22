f3d2ba2a052560594e89f8f1784779be80d0715b
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-12-22 21:33:12 +0530

    takeUForward/google/ClosestBinarySearchTreeValue.java

---
+++ takeUForward/google/ClosestBinarySearchTreeValue.java
@@ -0,0 +1,38 @@
+package takeUForward.google;
+
+public class ClosestBinarySearchTreeValue {
+    class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+
+    public int closestValue(TreeNode root, double target) {
+        int [] res = new int[1];
+        double [] diff = new double[1];
+        res[0] = -1;
+        diff[0] = Double.MAX_VALUE;
+        helper(root, target, res, diff);
+        return res[0];
+    }
+
+    private void helper(TreeNode root, double target, int[] res, double[] diff) {
+       if ( root == null) return ;
+       if ( root.val == target) {
+           res[0] = (int) target;
+           return;
+       }
+       if ( diff[0] >= Math.abs(root.val - target)){
+           diff[0] = Math.abs(root.val - target);
+           res[0] = Math.min(res[0], root.val);
+       }
+       helper(root.left, target, res, diff);
+        helper(root.right, target, res, diff);
+    }
+}
