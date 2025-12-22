4febd4ad39eb628e6af2c5ae0cc81291d73c7f6e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-12-22 21:33:12 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="Check If Two Expression Trees are Equivalent\n";
+        String s ="Closest Binary Search Tree Value\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
