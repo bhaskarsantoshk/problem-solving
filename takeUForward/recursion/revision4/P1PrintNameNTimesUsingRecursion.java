93e754a10d971d121a5dfd3c0ba55b947cfaad0f
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2025-12-31 14:28:05 +0530

    takeUForward/recursion/revision4/P1PrintNameNTimesUsingRecursion.java

--- takeUForward/recursion/revision4/P1PrintNameNTimesUsingRecursion.java
+++ takeUForward/recursion/revision4/P1PrintNameNTimesUsingRecursion.java
@@ -8,6 +8,8 @@ public class P1PrintNameNTimesUsingRecursion {
         int n = in.nextInt();
         System.out.println("Printing Name " + n+ " times");
         f(1, n);
+        System.out.println("Printing Name " + n+ " times using Tail Recursion");
+        printNameHeadRecursion(1, n);
     }
 
     private static void f(int i, int n) {
@@ -16,4 +18,11 @@ public class P1PrintNameNTimesUsingRecursion {
         f(i+1, n);
     }
     // T : O(N), S : O(N)
+
+    private static void printNameHeadRecursion(int count, int n){
+        if ( count == n)return;
+        count++;
+        f(count, n);
+        System.out.println("Batman");
+    }
 }
