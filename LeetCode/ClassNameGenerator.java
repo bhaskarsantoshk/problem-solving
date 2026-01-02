69e50ebbe06b82ac393fd49afb479bf3605665e1
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-01-02 23:18:17 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="Closest Binary Search Tree Value\n";
+        String s ="Rearrange array elements by sign\n\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
