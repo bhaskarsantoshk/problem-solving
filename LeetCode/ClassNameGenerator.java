b80339b61771386895228dfead74e9467a013bb7
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-01-05 11:32:23 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="Check if String is Palindrome or Not\n";
+        String s ="Check if a Number is Prime or Not\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
