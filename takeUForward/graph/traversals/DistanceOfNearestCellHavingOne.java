61d432ef68c4d3bacb18c74380a5a86271778e35
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-01-16 09:52:05 +0530

    takeUForward/graph/traversals/DistanceOfNearestCellHavingOne.java

---
+++ takeUForward/graph/traversals/DistanceOfNearestCellHavingOne.java
@@ -0,0 +1,8 @@
+package takeUForward.graph.traversals;
+
+public class DistanceOfNearestCellHavingOne {
+    public int[][] nearest(int[][] grid) {
+        // TODO: Finish this
+        return null;
+    }
+}
