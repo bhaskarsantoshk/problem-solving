f22b50eb536b10501319df03c7e2fa5876cc4132
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-01-22 08:58:29 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="Number of distinct islands\n";
+        String s ="16Sum of Digits in a Given Number\n\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
