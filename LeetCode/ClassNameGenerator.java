ff55e8c0b5a2d767ece00783c7ac8f0f719f7118
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-01-23 19:13:25 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="18Pow(x,n)\n";
+        String s ="19Generate Parentheses";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
