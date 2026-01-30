484a5ba776e8053a838f81c33c7337ba600254bf
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-01-30 11:37:38 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="Bipartite graph";
+        String s ="Topological sort or Kahn's algorithm";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
