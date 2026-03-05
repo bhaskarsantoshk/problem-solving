67fe3898364470c3005fd385ec689a43bbd6276e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-03-05 23:43:00 +0530

    company/apple/P26MaximumDepthOfBinaryTree.java

---
+++ company/apple/P26MaximumDepthOfBinaryTree.java
@@ -0,0 +1,10 @@
+package company.apple;
+
+public class P26MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if ( root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left,right)+1;
+    }
+}
