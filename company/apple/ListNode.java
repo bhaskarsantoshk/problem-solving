7dbd35f65c13e15b78cc4ba451fb2d10aac623f7
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-03-05 21:21:07 +0530

    company/apple/ListNode.java

---
+++ company/apple/ListNode.java
@@ -0,0 +1,15 @@
+package company.apple;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+
+    ListNode(int val){
+        this.val = val;
+    }
+}
