5269fc46784c863c700837bbd4ebed62e24f7b2c
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-03-24 22:31:55 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s =" 1929. Concatenation of Array";
+        String s =" 868. Binary Gap";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
