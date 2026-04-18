9fdcafc677eb28e1a0fb31b572ebcca8adc15ac5
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-04-18 14:58:04 +0530

    takeUForward/striver79/NextPermutation.java

--- takeUForward/striver79/NextPermutation.java
+++ takeUForward/striver79/NextPermutation.java
@@ -11,35 +11,29 @@ public class NextPermutation {
         Arrays.sort(tempArr);
         permute(all, tempArr, 0);
 
-        Set<List<Integer>> set = new LinkedHashSet<>(all);
-        all = new ArrayList<>(set);
-
-        // enforce lexicographic order
-        Collections.sort(all, (a, b) -> {
-            for (int i = 0; i < a.size(); i++) {
-                if (!a.get(i).equals(b.get(i))) {
-                    return a.get(i) - b.get(i);
-                }
-            }
-            return 0;
-        });
+       Collections.sort(all,
+               (a,b)-> {
+                   for (int i = 0; i < a.size(); i++) {
+                       int cmp = Integer.compare(a.get(i), b.get(i));
+                       if (cmp != 0) return cmp;
+                   }
+                   return 0;
+               });
 
         List<Integer> cur = new ArrayList<>();
         for (int num : original) cur.add(num);
 
+        int pos = -1;
         for (int i = 0; i < all.size(); i++) {
             if (all.get(i).equals(cur)) {
-                int nextIndex = (i == all.size() - 1) ? 0 : i + 1;
-
-                for (int j = 0; j < nums.length; j++) {
-                    nums[j] = all.get(nextIndex).get(j);
-                }
-                return;
+                pos = i; // keep updating → last occurrence
             }
         }
 
+        int nextIndex = (pos == all.size() - 1) ? 0 : pos + 1;
+
         for (int j = 0; j < nums.length; j++) {
-            nums[j] = all.get(0).get(j);
+            nums[j] = all.get(nextIndex).get(j);
         }
     }
 
@@ -61,5 +55,38 @@ public class NextPermutation {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+    public void nextPermutationOptimized(int[] nums) {
+        // find break point
+        // 4 3 2 8 9 7 6 5 4 3 2 1
+        int index = -1;
+        int n = nums.length;
+        for ( int i=n-2; i>=0; i--){
+            if ( nums[i] < nums[i+1]) {
+                index = i;
+                break;
+            }
+        }
+        if ( index == -1) {
+            reverse(nums, 0, n-1);
+            return;
+        }
+
+        for ( int i=n-1; i>=index; i--){
+            if ( nums[i] > nums[index]){
+                swap(nums, i, index);
+                break;
+            }
+        }
+        reverse(nums, index+1, n-1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end){
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
     }
 }
