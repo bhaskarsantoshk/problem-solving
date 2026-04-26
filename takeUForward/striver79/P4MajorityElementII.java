a652643a1c7a0924a2606297004b1a80dd52ed0c
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-04-26 15:47:16 +0530

    takeUForward/striver79/P4MajorityElementII.java

--- takeUForward/striver79/P4MajorityElementII.java
+++ takeUForward/striver79/P4MajorityElementII.java
@@ -1,7 +1,6 @@
 package takeUForward.striver79;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class P4MajorityElementII {
     public List<Integer> majorityElementTwo(int[] nums) {
@@ -14,6 +13,22 @@ public class P4MajorityElementII {
             }
             if ( list.size() == 2) break;
         }
+        return list;
+    }
+
+    public List<Integer> majorityElementTwoBetter(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        int min = nums.length/3+1;
+        Map<Integer, Integer> map = new HashMap<>();
+        for ( int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()){
+            if ( entry.getValue() >= min) list.add(entry.getKey());
+            if ( list.size() == 2) break;
+        }
+        Collections.sort(list);
         return list;
     }
 }
