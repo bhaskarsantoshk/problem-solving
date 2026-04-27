3f0a8dc9e4e16c4391ff8ddb4e2ea0ba04ffbebd
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-04-27 23:10:03 +0530

    takeUForward/striver79/P8CountInversions.java

--- takeUForward/striver79/P8CountInversions.java
+++ takeUForward/striver79/P8CountInversions.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class P8CountInversions {
+
     public long numberOfInversions(int[] nums) {
         int count = 0;
         for ( int i=0; i<nums.length; i++){
@@ -11,6 +12,12 @@ public class P8CountInversions {
                 if ( nums[i] > nums[j]) count++;
             }
         }
+        return count;
+    }
+
+    long count = 0;
+    public long numberOfInversionsOptimal(int[] nums) {
+        mergeSort(nums, 0, nums.length-1);
         return count;
     }
 
@@ -28,7 +35,8 @@ public class P8CountInversions {
         int mid = (low + high)/2;
         mergeSort(nums, low, mid);
         mergeSort(nums, mid+1, high);
-        merge(nums, low, mid, high);
+        //merge(nums, low, mid, high);
+        mergeModified(nums, low, mid, high);
     }
 
     private void merge(int[] nums, int low, int mid, int high) {
@@ -37,6 +45,27 @@ public class P8CountInversions {
         while ( left <= mid && right <= high){
             if ( nums[left] <= nums[right]) temp.add(nums[left++]);
             else temp.add(nums[right++]);
+        }
+        while ( left <= mid) temp.add(nums[left++]);
+        while (  right <= high) temp.add(nums[right++]);
+
+        for ( int i=low; i<=high; i++){
+            nums[i] = temp.get(i-low);
+        }
+    }
+
+    // modified
+
+    private void mergeModified(int[] nums, int low, int mid, int high) {
+        List<Integer> temp = new ArrayList<>();
+        int left = low, right = mid+1;
+        while ( left <= mid && right <= high){
+            if ( nums[left] <= nums[right]) temp.add(nums[left++]);
+            // Right is smaller
+            else {
+                temp.add(nums[right++]);
+                count+= (mid-left+1);
+            }
         }
         while ( left <= mid) temp.add(nums[left++]);
         while (  right <= high) temp.add(nums[right++]);
