fa5d78a7781639040962b7f913f6fb0ec694b282
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-04-27 22:55:04 +0530

    takeUForward/striver79/P8CountInversions.java

--- takeUForward/striver79/P8CountInversions.java
+++ takeUForward/striver79/P8CountInversions.java
@@ -1,5 +1,8 @@
 package takeUForward.striver79;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class P8CountInversions {
     public long numberOfInversions(int[] nums) {
         int count = 0;
@@ -9,5 +12,37 @@ public class P8CountInversions {
             }
         }
         return count;
+    }
+
+
+
+    // Pre-requisite
+
+    public int[] mergeSort(int[] nums) {
+        mergeSort(nums, 0, nums.length-1);
+        return nums;
+    }
+
+    private void mergeSort(int[] nums, int low, int high) {
+        if ( low == high) return;
+        int mid = (low + high)/2;
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid+1, high);
+        merge(nums, low, mid, high);
+    }
+
+    private void merge(int[] nums, int low, int mid, int high) {
+        List<Integer> temp = new ArrayList<>();
+        int left = low, right = mid+1;
+        while ( left <= mid && right <= high){
+            if ( nums[left] <= nums[right]) temp.add(nums[left++]);
+            else temp.add(nums[right++]);
+        }
+        while ( left <= mid) temp.add(nums[left++]);
+        while (  right <= high) temp.add(nums[right++]);
+
+        for ( int i=low; i<=high; i++){
+            nums[i] = temp.get(i-low);
+        }
     }
 }
