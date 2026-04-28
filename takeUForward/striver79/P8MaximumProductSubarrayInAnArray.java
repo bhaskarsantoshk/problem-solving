e2972234496986f3d794838dbda28a8efee68ff5
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-04-28 23:50:14 +0530

    takeUForward/striver79/P8MaximumProductSubarrayInAnArray.java

--- takeUForward/striver79/P8MaximumProductSubarrayInAnArray.java
+++ takeUForward/striver79/P8MaximumProductSubarrayInAnArray.java
@@ -30,4 +30,18 @@ public class P8MaximumProductSubarrayInAnArray {
         }
         return max;
     }
+
+    public int maxProductOneLoop(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int prefix = 1;
+        int suffix = 1;
+        for ( int i=0; i<nums.length; i++){
+            if ( prefix == 0) prefix = 1;
+            if ( suffix == 0) suffix = 1;
+            prefix *= nums[i];
+            suffix *= nums[nums.length-i-1];
+            max = Math.max(max, Math.max(prefix, suffix));
+        }
+        return max;
+    }
 }
