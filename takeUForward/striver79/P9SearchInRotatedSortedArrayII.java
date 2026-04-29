7f9e0a58c3b56f993196a1dd565d3b7bf5b6d67a
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-04-30 00:01:13 +0530

    takeUForward/striver79/P9SearchInRotatedSortedArrayII.java

--- takeUForward/striver79/P9SearchInRotatedSortedArrayII.java
+++ takeUForward/striver79/P9SearchInRotatedSortedArrayII.java
@@ -5,4 +5,81 @@ public class P9SearchInRotatedSortedArrayII {
         for ( int num: nums) if ( num == k) return true;
         return false;
     }
+
+
+    public boolean searchInARotatedSortedArrayII(int[] nums, int k) {
+        int low = 0, high = nums.length-1;
+
+        while ( low <= high){
+            int mid = low + (high-low)/2;
+
+            if ( nums[mid] == k) return true;
+            // example where this may fail 3 1 2 3 3 3 3
+            // in order to fix this
+            if ( nums[low] ==  nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+                continue;
+            }
+
+            // left half sorted?
+            if ( nums[low] <= nums[mid]){
+                if ( nums[low] <= k && nums[mid] >= k){
+                    high = mid-1;
+                } else {
+                    low = mid+1;
+                }
+
+                // right half sorted ?
+            } else {
+                if ( nums[mid] <= k && nums[high] >= k){
+                    low = mid+1;
+                } else {
+                    high = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+
+    // nums = [7, 8, 1, 2, 3, 4, 5, 6], k = 3
+    //
+
+    // When there are no duplicates this works
+
+
+    public boolean searchInARotatedSortedArray(int[] nums, int k) {
+       int low = 0, high = nums.length-1;
+
+       while ( low <= high){
+           int mid = low + (high-low)/2;
+
+           if ( nums[mid] == k) return true;
+           // example where this may fail 3 1 2 3 3 3 3
+           // in order to fix this
+           if ( nums[low] ==  nums[mid] && nums[mid] == nums[high]) {
+               low++;
+               high--;
+               continue;
+           }
+
+           // left half sorted?
+           if ( nums[low] <= nums[mid]){
+               if ( nums[low] <= k && nums[mid] >= k){
+                   high = mid-1;
+               } else {
+                   low = mid+1;
+               }
+
+           // right half sorted ?
+           } else {
+               if ( nums[mid] <= k && nums[high] >= k){
+                   low = mid+1;
+               } else {
+                   high = mid-1;
+               }
+           }
+       }
+       return false;
+    }
 }
