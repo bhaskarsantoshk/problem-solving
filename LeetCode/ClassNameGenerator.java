9163fc0157e431350793d3bb87e690d9f99f3777
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-04-30 23:38:03 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="9Search in rotated sorted array-II\n";
+        String s ="10Find minimum in Rotated Sorted Array\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
