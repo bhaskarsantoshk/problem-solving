d3d1c38079a68253185f14265da407e1dab80d76
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-05-29 00:38:19 +0530

    takeUForward/graph/revision3/P16DetectACycleInADirectedGraph.java

---
+++ takeUForward/graph/revision3/P16DetectACycleInADirectedGraph.java
@@ -0,0 +1,28 @@
+package takeUForward.graph.revision3;
+
+import java.util.List;
+
+public class P16DetectACycleInADirectedGraph {
+    public boolean isCyclic(int N, List<List<Integer>> adj) {
+        boolean[] vis = new boolean[N];
+        boolean[] pathVis = new boolean[N];
+        for ( int i=0; i<N; i++){
+            if ( !vis[i]){
+                if ( dfs(i, vis, pathVis, adj)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int i, boolean[] vis, boolean[] pathVis, List<List<Integer>> g) {
+        vis[i] = true;
+        pathVis[i] = true;
+        for ( int adjNode: g.get(i)){
+            if (!vis[adjNode]){
+                if ( dfs(adjNode, vis, pathVis, g)) return true;
+            } else if ( pathVis[adjNode]) return true;
+        }
+        pathVis[i] = false;
+        return false;
+    }
+}
