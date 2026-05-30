1652d541b4a9ace988a8a3c6741bd97321ed95ca
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-05-30 21:42:02 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="5Search in Linked List\n";
+        String s ="7Insert node before head in Doubly Linked List\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
