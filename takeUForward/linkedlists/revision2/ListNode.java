47a849a5b9ba2e19ed017cee8994c6c5f48a796e
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-05-30 20:58:06 +0530

    takeUForward/linkedlists/revision2/ListNode.java

---
+++ takeUForward/linkedlists/revision2/ListNode.java
@@ -0,0 +1,9 @@
+package takeUForward.linkedlists.revision2;
+
+public class ListNode {
+    public int data;
+    public ListNode next;
+    ListNode() { data = 0; next = null; }
+    ListNode(int x) { data = x; next = null; }
+    ListNode(int x, ListNode next) { data = x; this.next = next; }
+}
