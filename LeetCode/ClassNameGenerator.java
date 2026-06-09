4f28e1d6a598d2b2b597bac2e109b07f22c1575d
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-06-09 18:36:24 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="33Minimum multiplications to reach end\n";
+        String s ="34Bellman ford algorithm";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
