f5ac06655cb2a6f56f7fa3175167cd158a9557b6
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-06-12 13:59:27 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="787. Cheapest Flights Within K Stops\n";
+        String s ="200. Number of Islands";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
