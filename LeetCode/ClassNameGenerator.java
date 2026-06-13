6886175712f701b0958f103ce9785ecdbf177e98
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-06-13 11:23:47 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="209. Minimum Size Subarray Sum";
+        String s ="395. Longest Substring with At Least K Repeating Characters";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
