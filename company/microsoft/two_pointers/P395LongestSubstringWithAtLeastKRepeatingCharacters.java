3704e41f588725689fb5e75436c514881665d706
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-06-13 19:08:24 +0530

    company/microsoft/two_pointers/P395LongestSubstringWithAtLeastKRepeatingCharacters.java

--- company/microsoft/two_pointers/P395LongestSubstringWithAtLeastKRepeatingCharacters.java
+++ company/microsoft/two_pointers/P395LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -29,4 +29,57 @@ public class P395LongestSubstringWithAtLeastKRepeatingCharacters {
         }
         return charsK == chars;
     }
+
+    public int longestSubstringRecursion(String s, int k) {
+        return solve(s, 0, s.length(), k);
+    }
+
+    private int solve(String s, int l, int r, int k) {
+        if ( r - l < k) return 0;
+        int[] count = new int[26];
+        for ( int i=l; i<r; i++) count[s.charAt(i)-'a']++;
+        for ( int i=0; i<26; i++){
+            if ( count[i] > 0 & count[i] < k){
+                int bad = (char)('a'+i);
+                int start = l;
+                int ans =0;
+                for ( int j=l; j<r; j++){
+                    if ( s.charAt(j) ==bad){
+                        ans = Math.max(solve(s, start, j, k), ans);
+                        start = j+1;
+                    }
+                }
+                ans = Math.max(solve(s, start, r, k), ans);
+                return ans;
+            }
+        }
+        return r-l;
+    }
+
+    public int longestSubstring(String s, int k) {
+        int ans = 0;
+        int n = s.length();
+
+        for ( int targetUnique = 1; targetUnique <=26; targetUnique++){
+            int left = 0, right = 0;
+            int unique = 0, atleastK=0;
+            int[] count = new int[26];
+            while ( right < n){
+                char c = s.charAt(right);
+                if ( count[c-'a'] == 0) unique++;
+                count[c-'a']++;
+                if ( count[c-'a'] == k) atleastK++;
+                right++;
+                while ( unique > targetUnique){
+                    char d = s.charAt(left);
+                    if ( count[d-'a'] == k) atleastK--;
+                    count[d-'a']--;
+                    if ( count[d-'a'] == 0) unique--;
+                    left++;
+                }
+                if ( unique == targetUnique && unique == atleastK) ans = Math.max(ans, right-left);
+            }
+        }
+        return ans;
+    }
 }
