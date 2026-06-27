3f6fac31cce5175f2a54cda22eda4ee5db614f7b
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-06-27 14:56:03 +0530

    company/google/FindEventualSafeStates.java

--- company/google/FindEventualSafeStates.java
+++ company/google/FindEventualSafeStates.java
@@ -1,11 +1,9 @@
 package company.google;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class FindEventualSafeStates {
-    public int[] eventualSafeNodes(int V, int[][] adj) {
+    public int[] eventualSafeNodesDFS(int V, int[][] adj) {
         boolean[] vis = new boolean[V];
         boolean[] pathVis = new boolean[V];
         List<Integer> res = new ArrayList<>();
@@ -28,5 +26,39 @@ public class FindEventualSafeStates {
         pathVis[node] = false;
         res.add(node);
         return false;
+    }
+
+
+    public int[] eventualSafeNodesBFS(int V, int[][] adj) {
+        List<Integer>[] g = new ArrayList[V];
+        for ( int i=0; i<V; i++) g[i] = new ArrayList<>();
+        int[] indegree = new int[V];
+        for (int i=0; i<V; i++){
+            for ( int j=0; j<adj[i].length; j++){
+                int u = i;
+                int v = adj[i][j];
+                g[v].add(u);
+                indegree[u]++;
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for ( int i=0; i<V; i++) if (indegree[i] == 0) queue.offer(i);
+        List<Integer> res = new ArrayList<>();
+        while ( !queue.isEmpty()){
+            int node = queue.poll();
+            res.add(node);
+            for ( int adjNode: g[node]){
+                indegree[adjNode]--;
+                if ( indegree[adjNode] == 0) queue.offer(adjNode);
+            }
+        }
+
+        int[] ans = new int[res.size()];
+        for ( int i=0; i<res.size(); i++){
+            ans[i] = res.get(i);
+        }
+
+        Arrays.sort(ans);
+        return ans;
     }
 }
