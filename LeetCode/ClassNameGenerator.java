f68addb0b71fc9daf720d22539297ccc0af603bc
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-06-28 15:13:16 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="Alien Dictionary\n";
+        String s ="Shortest path in undirected graph with unit weights\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
