df426bd752605788436e24b0414223d9dfcd22fd
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-06-30 23:44:15 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="237. Delete Node in a Linked List\n";
+        String s ="    Maximum Distance-Value Product";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
