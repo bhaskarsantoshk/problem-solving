91e4016c1ef80a55e356cc8e2fb8ff08e151b239
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-06-30 23:19:05 +0530

    company/microsoft/P237DeleteNodeInALinkedList.java

---
+++ company/microsoft/P237DeleteNodeInALinkedList.java
@@ -0,0 +1,12 @@
+package company.microsoft;
+
+public class P237DeleteNodeInALinkedList {
+    public void deleteNode(ListNode node) {
+        if ( node == null) return;
+        if ( node.next == null){
+            node = null; return;
+        }
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
