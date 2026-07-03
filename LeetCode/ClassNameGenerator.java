d4b49f58ee75caed6e8563a845557aa91f9b3fb9
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-07-04 00:08:27 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="40Most stones removed with same row or column\n";
+        String s ="41Accounts merge\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
