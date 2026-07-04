4d1c4de151c0e487bc70345ec11b1d96ccc67a77
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-07-04 13:33:12 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="41Accounts merge\n";
+        String s ="47Articulation point in graph\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
