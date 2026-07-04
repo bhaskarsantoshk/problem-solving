664167b7b59f06fc93576e3905ad655a0d53600d
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-07-04 18:46:30 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="48Kosaraju's algorithm\n\n";
+        String s ="297. Serialize and Deserialize Binary Tree";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
