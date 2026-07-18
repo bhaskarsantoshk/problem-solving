921fe3229577b50b265dfd63166896cbd95f6c93
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-07-18 16:14:23 +0530

    takeUForward/graph/revision/DetectACycleInAnUndirectedGraph.java

--- takeUForward/graph/revision/DetectACycleInAnUndirectedGraph.java
+++ takeUForward/graph/revision/DetectACycleInAnUndirectedGraph.java
@@ -1,9 +1,11 @@
 package takeUForward.graph.revision;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DetectACycleInAnUndirectedGraph {
-    public boolean isCycle(int V, List<Integer>[] adj) {
+    public boolean isCycleDFS(int V, List<Integer>[] adj) {
         boolean[] vis = new boolean[V];
         for ( int i=0; i<V; i++){
             if (!vis[i]){
@@ -21,6 +23,34 @@ public class DetectACycleInAnUndirectedGraph {
             } else if ( adjNode != parent) return true;
         }
 
+        return false;
+    }
+
+    public boolean isCycleBFS(int V, List<Integer>[] adj) {
+        boolean[] vis = new boolean[V];
+        for ( int i=0; i<V; i++){
+            if (!vis[i]){
+                if ( isCycleBFS(i, adj, vis)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isCycleBFS(int node, List<Integer>[] g, boolean[] vis) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{node, -1});
+        vis[node] = true;
+        while (!queue.isEmpty()){
+            int nodeArr[] = queue.poll();
+            node = nodeArr[0];
+            int parent = nodeArr[1];
+            for ( int adjNode: g[node]){
+                if (!vis[adjNode]) {
+                    vis[adjNode] = true;
+                    queue.offer(new int[]{adjNode, node});
+                } else if ( adjNode != parent) return true;
+            }
+        }
         return false;
     }
 }
