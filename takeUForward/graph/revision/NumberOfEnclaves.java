a46e41dedcf791634a2757b7a69ca4539e0d8115
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-08-21 23:33:52 +0530

    takeUForward/graph/revision/NumberOfEnclaves.java

---
+++ takeUForward/graph/revision/NumberOfEnclaves.java
@@ -0,0 +1,11 @@
+package takeUForward.graph.revision;
+
+public class NumberOfEnclaves {
+    public int numberOfEnclaves(int[][] grid) {
+        return numberOfEnclavesDFS(grid);
+    }
+
+    private int numberOfEnclavesDFS(int[][] grid) {
+
+    }
+}
