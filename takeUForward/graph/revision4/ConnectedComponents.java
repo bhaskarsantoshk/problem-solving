8e0274d8d0f88b8124378499459c0637766f1c52
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-08-22 11:44:04 +0530

    takeUForward/graph/revision4/ConnectedComponents.java

---
+++ takeUForward/graph/revision4/ConnectedComponents.java
@@ -0,0 +1,34 @@
+package takeUForward.graph.revision4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectedComponents {
+    public int findNumberOfComponent(int V, List<List<Integer>> edges) {
+        List<Integer>[] g = new ArrayList[V];
+        for ( int i=0; i<V; i++) g[i] = new ArrayList<>();
+        for ( List<Integer> edge: edges){
+            int u = edge.get(0), v = edge.get(1);
+            g[u].add(v);
+            g[v].add(u);
+        }
+
+        int connectedComponents = 0;
+        boolean[] vis = new boolean[V];
+        for ( int i=0; i<V ; i++){
+            if ( !vis[i]) {
+                connectedComponents++;
+                dfs(i, g, vis);
+            }
+        }
+
+        return connectedComponents;
+    }
+
+    private void dfs(int node, List<Integer>[] g , boolean[] vis){
+        vis[node] = true;
+        for ( int adjNode: g[node]){
+            if (!vis[adjNode]) dfs(adjNode, g, vis);
+        }
+    }
+}
