620ee4328586c86ae3335917180c6204eb0f71d5
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-08-24 20:36:05 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="G20Find eventual safe statesDFS\n";
+        String s ="G20Find eventual safe statesBFS\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
