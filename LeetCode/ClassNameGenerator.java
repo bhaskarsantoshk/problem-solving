559f0d9eafd7e8a8c3428c6330aa69807c6b83d2
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-08-28 20:39:15 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="G26Shortest Distance in a Binary Maze\n";
+        String s ="G27Path with minimum effort\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
