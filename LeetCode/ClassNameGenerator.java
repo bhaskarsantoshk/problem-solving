82e4c416379bd672f33abd1beb6a5f93f501a9d7
bhaskarsantoshk <bhaskarsantoshk@gmail.com>
2026-09-12 11:37:34 +0530

    LeetCode/ClassNameGenerator.java

--- LeetCode/ClassNameGenerator.java
+++ LeetCode/ClassNameGenerator.java
@@ -31,7 +31,7 @@ public class ClassNameGenerator {
     }
 
     public static void main(String[] args){
-        String s ="G32Bellman ford algorithm\n";
+        String s ="G33Floyd warshall algorithm\n";
         // Expected Output : P392IsSubsequence
         System.out.println("P"+getClassName(s));
     }
